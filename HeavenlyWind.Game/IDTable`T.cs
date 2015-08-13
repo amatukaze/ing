ec2c2a1922cb50ab87e4f0c4fa2dcfb10c51dc87
神樹桜乃 <kodama@sakuno.moe>
2015-08-13 20:01:36 +0800

    + Base class, interface and ID table for game data models

---
+++ HeavenlyWind.Game/IDTable`T.cs
@@ -0,0 +1,73 @@
+﻿using Sakuno.KanColle.Amatsukaze.Game.Models;
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Sakuno.KanColle.Amatsukaze.Game
+{
+    public class IDTable<T> : IReadOnlyDictionary<int, T> where T : IID
+    {
+        IDictionary<int, T> r_Dictionary;
+        public IEnumerable<int> Keys { get { return r_Dictionary.Keys; } }
+        public IEnumerable<T> Values { get { return r_Dictionary.Values; } }
+        public int Count { get { return r_Dictionary.Count; } }
+        public T this[int rpKey]
+        {
+            get
+            {
+                T rResult;
+                if (r_Dictionary.TryGetValue(rpKey, out rResult))
+                    return rResult;
+
+                throw new KeyNotFoundException(rpKey.ToString());
+            }
+        }
+
+        public IDTable() : this(new Dictionary<int, T>()) { }
+        public IDTable(IEnumerable<T> rpSource) : this(rpSource.ToDictionary(r => r.ID)) { }
+        public IDTable(Dictionary<int, T> rpSource)
+        {
+            r_Dictionary = rpSource;
+        }
+
+        internal void Add(T rpData) => r_Dictionary.Add(rpData.ID, rpData);
+        internal void Remove(int rpID) => r_Dictionary.Remove(rpID);
+        internal void Remove(T rpData) => r_Dictionary.Remove(rpData.ID);
+        internal void Clear() => r_Dictionary.Clear();
+        public bool ContainsKey(int rpKey) => r_Dictionary.ContainsKey(rpKey);
+        public bool TryGetValue(int rpKey, out T ropValue) => r_Dictionary.TryGetValue(rpKey, out ropValue);
+        
+        public IEnumerator<KeyValuePair<int, T>> GetEnumerator() => r_Dictionary.GetEnumerator();
+        IEnumerator IEnumerable.GetEnumerator() => GetEnumerator();
+
+        public bool UpdateRawData<TRawData>(IEnumerable<TRawData> rpRawDataCollection, Func<TRawData, T> rpValueFactory, Action<T, TRawData> rpUpdate)
+            where TRawData : IID
+        {
+            var rResult = false;
+
+            var rRemovedIDs = new HashSet<int>(r_Dictionary.Keys);
+            foreach (var rRawData in rpRawDataCollection)
+            {
+                rRemovedIDs.Remove(rRawData.ID);
+
+                T rData;
+                if (rpUpdate != null && r_Dictionary.TryGetValue(rRawData.ID, out rData))
+                    rpUpdate(rData, rRawData);
+                else
+                {
+                    Add(rpValueFactory(rRawData));
+                    rResult = true;
+                }
+            }
+
+            foreach (var rID in rRemovedIDs)
+            {
+                Remove(rID);
+                rResult = true;
+            }
+
+            return rResult;
+        }
+    }
+}
---
+++ HeavenlyWind.Game/Models/RawDataWrapper`T.cs
@@ -0,0 +1,21 @@
+﻿namespace Sakuno.KanColle.Amatsukaze.Game.Models
+{
+    public abstract class RawDataWrapper<T> : ModelBase
+    {
+        internal T RawData { get; private set; }
+
+        protected RawDataWrapper(T rpRawData)
+        {
+            Update(rpRawData);
+        }
+
+        public void Update(T rpRawData)
+        {
+            RawData = rpRawData;
+            OnRawDataUpdated();
+        }
+        protected virtual void OnRawDataUpdated()
+        {
+        }
+    }
+}
