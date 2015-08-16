14d23b89673d10fa351a819a8f8823e5558b5a57
神樹桜乃 <kodama@sakuno.moe>
2015-08-16 13:42:19 +0800

    + Basic i18n support

---
+++ HeavenlyWind.Base/StringResources.Items.cs
@@ -0,0 +1,11 @@
+﻿// This is an auto-generated file
+
+namespace Sakuno.KanColle.Amatsukaze
+{
+    partial class StringResourcesItems
+    {
+
+        public string Log_Welcome => GetString(nameof(Log_Welcome));
+
+    }
+}
---
+++ HeavenlyWind.Base/StringResources.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.Linq;
+using System.Linq;
+using System.Collections.Generic;
+
+namespace Sakuno.KanColle.Amatsukaze
+{
+    public class StringResources : ModelBase
+    {
+        public static StringResources Instance { get; } = new StringResources();
+
+        static string StringResourceDirectory;
+
+        StringResourcesItems r_Main;
+        public StringResourcesItems Main
+        {
+            get { return r_Main; }
+            private set
+            {
+                if (r_Main != value)
+                {
+                    r_Main = value;
+                    OnPropertyChanged(nameof(Main));
+                }
+            }
+        }
+
+        StringResources()
+        {
+            var rRootDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+            StringResourceDirectory = Path.Combine(rRootDirectory, "Resources", "Strings");
+        }
+
+        public void Load(string rpLanguageName)
+        {
+            var rMainResourceFile = Path.Combine(StringResourceDirectory, rpLanguageName, "Main.xml");
+            if (!File.Exists(rMainResourceFile))
+                throw new Exception();
+            
+            Main = new StringResourcesItems(XDocument.Load(rMainResourceFile).Root.Descendants("String").ToDictionary(r => r.Attribute("Key").Value, r => r.Value));
+        }
+    }
+    
+    public partial class StringResourcesItems
+    {
+        Dictionary<string, string> r_Items;
+
+        internal StringResourcesItems(Dictionary<string, string> rpItems)
+        {
+            r_Items = rpItems;
+        }
+
+        public string GetString(string rpKey)
+        {
+            string rResult;
+            if (!r_Items.TryGetValue(rpKey, out rResult))
+                rResult = rpKey;
+
+            return rResult;
+        }
+    }
+}
