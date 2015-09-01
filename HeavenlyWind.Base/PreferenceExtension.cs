bedcde11f33fb7efb4ed11dc83380895f0ec0990
神樹桜乃 <kodama@sakuno.moe>
2015-09-01 10:12:34 +0800

    + Two markup extensions

---
+++ HeavenlyWind.Base/PreferenceExtension.cs
@@ -0,0 +1,19 @@
+﻿using Sakuno.KanColle.Amatsukaze.Models;
+using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Sakuno.KanColle.Amatsukaze
+{
+    public class PreferenceExtension : MarkupExtension
+    {
+        string r_Path;
+
+        public PreferenceExtension(string rpPath)
+        {
+            r_Path = rpPath;
+        }
+
+        public override object ProvideValue(IServiceProvider rpServiceProvider) => new Binding(r_Path) { Source = Preference.Current, Mode = BindingMode.TwoWay }.ProvideValue(rpServiceProvider);
+    }
+}
---
+++ HeavenlyWind.Base/StringResourceExtension.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Windows.Data;
+using System.Windows.Markup;
+
+namespace Sakuno.KanColle.Amatsukaze
+{
+    public class StringResourceExtension : MarkupExtension
+    {
+        string r_Path;
+
+        public StringResourceExtension(string rpPath)
+        {
+            r_Path = rpPath;
+        }
+
+        public override object ProvideValue(IServiceProvider rpServiceProvider)
+        {
+            if (!StringResources.Instance.IsLoaded)
+                return r_Path;
+
+            return new Binding(r_Path) { Source = StringResources.Instance }.ProvideValue(rpServiceProvider);
+        }
+    }
+}
