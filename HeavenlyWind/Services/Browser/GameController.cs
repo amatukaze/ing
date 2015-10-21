0a7cc1ca62891fbe449e26567a2ee4a4e185c0d7
神樹桜乃 <kodama@sakuno.moe>
2015-10-21 21:52:41 +0800

    Filter the session which is not created by the browser process

--- HeavenlyWind/Services/Browser/GameController.cs
+++ HeavenlyWind/Services/Browser/GameController.cs
@@ -1,4 +1,7 @@
 ﻿using Sakuno.SystemInterop;
+using System;
+using System.Diagnostics;
+using System.Management;
 using System.Windows.Input;
 
 namespace Sakuno.KanColle.Amatsukaze.Services.Browser
@@ -24,11 +27,43 @@ namespace Sakuno.KanColle.Amatsukaze.Services.Browser
         public GameController()
         {
             if (OS.IsWin7OrLater)
+            {
+                foreach (var rSession in VolumeManager.Instance.EnumerateSessions())
+                    rSession.Dispose();
+
                 VolumeManager.Instance.NewSession += r =>
                 {
+                    var rHostProcessID = Process.GetCurrentProcess().Id;
+                    int? rProcessID = r.ProcessID;
+
+                    var rIsBrowserProcess = false;
+
+                    while (rProcessID.HasValue)
+                        using (var rManagementObject = new ManagementObject($"Win32_Process.Handle='{rProcessID.Value}'"))
+                            try
+                            {
+                                rManagementObject.Get();
+                                rProcessID = Convert.ToInt32(rManagementObject["ParentProcessId"]);
+
+                                if (rProcessID == rHostProcessID)
+                                {
+                                    rIsBrowserProcess = true;
+                                    break;
+                                }
+                            }
+                            catch (ManagementException e) when (e.ErrorCode == ManagementStatus.NotFound)
+                            {
+                                rProcessID = null;
+                            }
+
+                    if (!rIsBrowserProcess)
+                        return;
+
                     Volume?.Dispose();
                     Volume = new BrowserVolume(r);
                 };
+
+            }
 
             RestartGameCommand = new DelegatedCommand(RestartGame);
         }
