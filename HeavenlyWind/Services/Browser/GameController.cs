6b1b3999c93f089f4d5fc950cc979c27d717e317
神樹桜乃 <kodama@sakuno.moe>
2015-10-21 17:13:45 +0800

    Basic volume manager

--- HeavenlyWind/Services/Browser/GameController.cs
+++ HeavenlyWind/Services/Browser/GameController.cs
@@ -1,13 +1,35 @@
-﻿using System.Windows.Input;
+﻿using Sakuno.SystemInterop;
+using System.Windows.Input;
 
 namespace Sakuno.KanColle.Amatsukaze.Services.Browser
 {
     class GameController : ModelBase
     {
+        BrowserVolume r_Volume;
+        public BrowserVolume Volume
+        {
+            get { return r_Volume; }
+            private set
+            {
+                if (r_Volume != value)
+                {
+                    r_Volume = value;
+                    OnPropertyChanged(nameof(Volume));
+                }
+            }
+        }
+
         public ICommand RestartGameCommand { get; }
 
         public GameController()
         {
+            if (OS.IsWin7OrLater)
+                VolumeManager.Instance.NewSession += r =>
+                {
+                    Volume?.Dispose();
+                    Volume = new BrowserVolume(r);
+                };
+
             RestartGameCommand = new DelegatedCommand(RestartGame);
         }
 
