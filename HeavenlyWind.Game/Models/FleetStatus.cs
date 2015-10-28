9c74c6743a1718c43869b9e4349039dd9b3e8512
神樹桜乃 <kodama@sakuno.moe>
2015-10-28 22:36:23 +0800

    New AA calculation

--- HeavenlyWind.Game/Models/FleetStatus.cs
+++ HeavenlyWind.Game/Models/FleetStatus.cs
@@ -5,6 +5,9 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models
 {
     public class FleetStatus : ModelBase
     {
+        static int[] r_CarrierBasedFighterAABouns = { 0, 0, 2, 5, 9, 14, 14, 22 };
+        static int[] r_SeaplaneBomberAABouns = { 0, 0, 1, 1, 1, 3, 3, 6 };
+
         Fleet r_Fleet;
 
         int r_TotalLevel;
@@ -21,16 +24,29 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models
             }
         }
 
-        int r_AA;
-        public int AA
+        double r_MinAA;
+        public double MinAA
         {
-            get { return r_AA; }
+            get { return r_MinAA; }
             private set
             {
-                if (r_AA != value)
+                if (r_MinAA != value)
                 {
-                    r_AA = value;
-                    OnPropertyChanged(nameof(AA));
+                    r_MinAA = value;
+                    OnPropertyChanged(nameof(MinAA));
+                }
+            }
+        }
+        double r_MaxAA;
+        public double MaxAA
+        {
+            get { return r_MaxAA; }
+            private set
+            {
+                if (r_MaxAA != value)
+                {
+                    r_MaxAA = value;
+                    OnPropertyChanged(nameof(MaxAA));
                 }
             }
         }
@@ -47,16 +63,35 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models
         {
             TotalLevel = r_Fleet.Ships.Sum(r => r.Level);
 
-            AA = r_Fleet.Ships.Sum(rpShip =>
-                rpShip.Slots.Where(r => r.HasEquipment).Sum(rpSlot =>
-                {
-                    if (rpSlot.Equipment.Info.CanParticipateInFighterCombat)
-                        return (int)(rpSlot.Equipment.Info.AA * Math.Sqrt(rpSlot.PlaneCount));
-                    else
-                        return 0;
-                }));
+            MinAA = CalculateAA(r => r == 0 ? .0 : (r - 1) * 15.0 + 10.0);
+            MaxAA = CalculateAA(r => r == 9 ? .0 : (r - 1) * 15.0 + 24.0);
 
             LoS.Update();
         }
+        double CalculateAA(Func<int, double> rpInternalBouns) =>
+            r_Fleet.Ships.Sum(rpShip =>
+                    rpShip.Slots.Where(r => r.HasEquipment).Sum(rpSlot =>
+                    {
+                        if (!rpSlot.Equipment.Info.CanParticipateInFighterCombat)
+                            return .0;
+                        else
+                        {
+                            var rResult = rpSlot.Equipment.Info.AA * Math.Sqrt(rpSlot.PlaneCount);
+
+                            if (rpSlot.PlaneCount > 0)
+                            {
+                                var rProficiency = rpSlot.Equipment.Proficiency;
+
+                                if (rpSlot.Equipment.Info.Type == EquipmentType.CarrierBasedFighter)
+                                    rResult += r_CarrierBasedFighterAABouns[rProficiency];
+                                else if (rpSlot.Equipment.Info.Type == EquipmentType.SeaplaneBomber)
+                                    rResult += r_SeaplaneBomberAABouns[rProficiency];
+
+                                rResult += Math.Sqrt(rpInternalBouns(rProficiency) / 10.0);
+                            }
+
+                            return rResult;
+                        }
+                    }));
     }
 }
