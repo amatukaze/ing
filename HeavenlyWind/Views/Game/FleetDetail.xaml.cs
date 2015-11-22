5eb9f9bd8f6e757b862cd32eabaa2d270c7da70d
神樹桜乃 <kodama@sakuno.moe>
2015-11-22 20:08:41 +0800

    Use dragable tab for fleets panel

---
+++ HeavenlyWind/Views/Game/FleetDetail.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sakuno.KanColle.Amatsukaze.Views.Game
+{
+    /// <summary>
+    /// FleetDetail.xaml の相互作用ロジック
+    /// </summary>
+    public partial class FleetDetail : UserControl
+    {
+        public FleetDetail()
+        {
+            InitializeComponent();
+        }
+    }
+}
