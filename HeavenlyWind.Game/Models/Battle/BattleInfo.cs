9e93ef2717bad624e8a88dac0435ab095457ec95
神樹桜乃 <kodama@sakuno.moe>
2015-12-06 17:41:04 +0800

    Something

--- HeavenlyWind.Game/Models/Battle/BattleInfo.cs
+++ HeavenlyWind.Game/Models/Battle/BattleInfo.cs
@@ -1,4 +1,7 @@
-﻿using System;
+using Sakuno.KanColle.Amatsukaze.Game.Models.Raw.Battle;
+using Sakuno.KanColle.Amatsukaze.Game.Parsers;
+using Sakuno.KanColle.Amatsukaze.Game.Services;
+using System;
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle
 {
@@ -22,7 +25,47 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle
 
         public BattleResult Result { get; } = new BattleResult();
 
+        static BattleInfo()
+        {
+            SessionService.Instance.Subscribe("api_port/port", _ => r_Current = null);
+
+            var rFirstStages = new[]
+            {
+                "api_req_sortie/battle",
+                "api_req_battle_midnight/sp_midnight",
+                "api_req_sortie/airbattle",
+                "api_req_combined_battle/airbattle",
+                "api_req_combined_battle/battle",
+                "api_req_combined_battle/battle_water",
+                "api_req_combined_battle/sp_midnight",
+            };
+            SessionService.Instance.Subscribe(rFirstStages, r => r_Current?.ProcessFirstStage(r));
+
+            var rSecondStages = new[]
+            {
+                "api_req_battle_midnight/battle",
+                "api_req_combined_battle/midnight_battle",
+            };
+            SessionService.Instance.Subscribe(rSecondStages, r => r_Current?.ProcessSecondStage(r));
+        }
         internal BattleInfo()
+        {
+            r_Current = this;
+        }
+
+        void ProcessFirstStage(ApiData rpData)
+        {
+            SetFormationAndEngagementForm(rpData);
+        void SetFormationAndEngagementForm(ApiData rpData)
+        {
+            var rFormationRawData = rpData.Data as IRawFormationAndEngagementForm;
+
+            FriendFormation = (Formation)rFormationRawData.FormationAndEngagementForm[0];
+            EnemyFormation = (Formation)rFormationRawData.FormationAndEngagementForm[1];
+            EngagementForm = (EngagementForm)rFormationRawData.FormationAndEngagementForm[2];
+        }
+
+        void ProcessSecondStage(ApiData rpData)
         {
         }
     }
--- HeavenlyWind.Game/Models/CombinedFleetType.cs
+++ HeavenlyWind.Game/Models/CombinedFleetType.cs
@@ -5,5 +5,6 @@
         None,
         CarrierTaskForce,
         SurfaceTaskForce,
+        TransportEscort,
     }
 }
