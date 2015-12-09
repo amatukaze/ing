71926db4b0cc60ac3f7b1e80046d26e3c1d53c4c
神樹桜乃 <kodama@sakuno.moe>
2015-12-09 13:33:12 +0800

    Status of combined fleet

--- HeavenlyWind.Game/Models/Battle/BattleInfo.cs
+++ HeavenlyWind.Game/Models/Battle/BattleInfo.cs
@@ -59,6 +59,11 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle
             var rFleets = KanColleGame.Current.Port.Fleets;
             if (rFleets.CombinedFleetType == 0)
                 Participants.FriendMain = KanColleGame.Current.Sortie.Fleet.Ships.Select(r => new FriendShip(r)).ToList<IParticipant>();
+            else
+            {
+                Participants.FriendMain = rFleets[1].Ships.Select(r => new FriendShip(r)).ToList<IParticipant>().AsReadOnly();
+                Participants.FriendEscort = rFleets[2].Ships.Select(r => new FriendShip(r)).ToList<IParticipant>().AsReadOnly();
+            }
 
             OnPropertyChanged(nameof(CurrentStage));
         }
