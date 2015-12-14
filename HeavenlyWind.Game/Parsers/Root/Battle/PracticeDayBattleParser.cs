989debb824dcb7575eb3a2799bb7119fd4df8a55
神樹桜乃 <kodama@sakuno.moe>
2015-12-15 00:10:51 +0800

    Show practice battle information

---
+++ HeavenlyWind.Game/Parsers/Root/Battle/PracticeDayBattleParser.cs
@@ -0,0 +1,22 @@
+﻿using Sakuno.KanColle.Amatsukaze.Game.Models;
+using Sakuno.KanColle.Amatsukaze.Game.Models.Battle;
+using Sakuno.KanColle.Amatsukaze.Game.Models.Raw.Battle;
+
+namespace Sakuno.KanColle.Amatsukaze.Game.Parsers.Root.Battle
+{
+    [Api("api_req_practice/battle")]
+
+    class PracticeDayBattleParser : DayNormalBattleParser
+    {
+        public override void Process(RawDay rpData)
+        {
+            var rPracticeInfo = Game.Sortie as PracticeInfo;
+            if (rPracticeInfo != null)
+            {
+                var rParticipantFleet = Game.Port.Fleets[int.Parse(Requests["api_deck_id"])];
+
+                rPracticeInfo.Battle = new BattleInfo(rParticipantFleet);
+            }
+        }
+    }
+}
