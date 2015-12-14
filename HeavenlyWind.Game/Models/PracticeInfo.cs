0c833e63a2f49491deeff9ff42481c6314080774
神樹桜乃 <kodama@sakuno.moe>
2015-12-14 21:39:08 +0800

    Show opponent information

---
+++ HeavenlyWind.Game/Models/PracticeInfo.cs
@@ -0,0 +1,35 @@
+﻿using Sakuno.KanColle.Amatsukaze.Game.Models.Battle;
+using Sakuno.KanColle.Amatsukaze.Game.Models.Raw;
+using Sakuno.KanColle.Amatsukaze.Game.Services;
+
+namespace Sakuno.KanColle.Amatsukaze.Game.Models
+{
+    public class PracticeInfo : SortieInfo
+    {
+        static PracticeInfo r_Current;
+
+        public PracticeOpponentInfo Opponent { get; }
+
+        BattleInfo r_Battle;
+        public BattleInfo Battle
+        {
+            get { return r_Battle; }
+            internal set
+            {
+                r_Battle = value;
+                OnPropertyChanged(nameof(Battle));
+            }
+        }
+
+        static PracticeInfo()
+        {
+            SessionService.Instance.Subscribe("api_port/port", _ => r_Current = null);
+        }
+        internal PracticeInfo(RawPracticeOpponentInfo rpRawData)
+        {
+            r_Current = this;
+
+            Opponent = new PracticeOpponentInfo(rpRawData);
+        }
+    }
+}
---
+++ HeavenlyWind.Game/Models/Raw/RawPracticeOpponentInfo.cs
@@ -0,0 +1,55 @@
+﻿using Newtonsoft.Json;
+
+namespace Sakuno.KanColle.Amatsukaze.Game.Models.Raw
+{
+    public class RawPracticeOpponentInfo
+    {
+        [JsonProperty("api_member_id")]
+        public int ID { get; set; }
+        [JsonProperty("api_nickname")]
+        public string Name { get; set; }
+        [JsonProperty("api_cmt")]
+        public string Comment { get; set; }
+
+        [JsonProperty("api_level")]
+        public int Level { get; set; }
+        [JsonProperty("api_rank")]
+        public AdmiralRank Rank { get; set; }
+        [JsonProperty("api_experience")]
+        public int[] Experience { get; set; }
+
+        [JsonProperty("api_friend")]
+        public int FleetCount { get; set; }
+        [JsonProperty("api_ship")]
+        public int[] ShipCount { get; set; }
+        [JsonProperty("api_slotitem")]
+        public int[] EquipmentCount { get; set; }
+        [JsonProperty("api_furniture")]
+        public int FurnitureCount { get; set; }
+
+        [JsonProperty("api_deckname")]
+        public string FleetName { get; set; }
+        [JsonProperty("api_deck")]
+        public RawFleet Fleet { get; set; }
+
+        public class RawFleet
+        {
+            [JsonProperty("api_ships")]
+            public RawShip[] Ships { get; set; }
+        }
+        public class RawShip
+        {
+            [JsonProperty("api_id")]
+            public int ID { get; set; }
+
+            [JsonProperty("api_ship_id")]
+            public int ShipID { get; set; }
+
+            [JsonProperty("api_level")]
+            public int Level { get; set; }
+
+            //[JsonProperty("api_star")]
+        }
+    }
+
+}
