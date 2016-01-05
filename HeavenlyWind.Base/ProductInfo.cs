d08f376fa76e6d9bb59235f03bff3e535f2d2197
神樹桜乃 <kodama@sakuno.moe>
2016-01-05 14:11:19 +0800

    Apply new name to some places

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -2,6 +2,9 @@
 {
     public class ProductInfo
     {
+        public const string AppName = "KanColle Inspector";
+        public const string FullAppName = "KanColle Inspector ~ HeavenlyWind";
+
         public const string AssemblyVersionString = "0.1.3";
 
         public const string ReleaseCodeName = "Serpentina";
