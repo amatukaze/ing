aa485ba932944de7d502681b7edc04aa0fb56c56
神樹桜乃 <kodama@sakuno.moe>
2016-01-05 16:27:25 +0800

    Version 0.1.4 (Pipiro)

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -5,9 +5,9 @@
         public const string AppName = "KanColle Inspector";
         public const string FullAppName = "KanColle Inspector ~ HeavenlyWind";
 
-        public const string AssemblyVersionString = "0.1.3";
+        public const string AssemblyVersionString = "0.1.4";
 
-        public const string ReleaseCodeName = "Serpentina";
+        public const string ReleaseCodeName = "Pipiro";
 
         public const string UserAgent = "KanColleInspector/" + AssemblyVersionString;
     }
