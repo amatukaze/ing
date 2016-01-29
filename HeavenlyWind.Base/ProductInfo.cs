ecc3c345c6aeed748d442ffd6c0e727ecedf918d
神樹桜乃 <kodama@sakuno.moe>
2016-01-29 12:46:07 +0000

    Version 0.1.5 (Traum)

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -5,7 +5,7 @@
         public const string AppName = "KanColle Inspector";
         public const string FullAppName = "KanColle Inspector ~ HeavenlyWind";
 
-        public const string AssemblyVersionString = "0.1.5";
+        public const string AssemblyVersionString = "0.1.5.1";
 
         public const string ReleaseCodeName = "Traum";
 
