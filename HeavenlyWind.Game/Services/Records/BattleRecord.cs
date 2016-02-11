014c8a544814826e4588c327ca9c3f8a2bf8fdcd
神樹桜乃 <kodama@sakuno.moe>
2016-02-11 15:59:43 +0800

    Something awkward

--- HeavenlyWind.Game/Services/Records/BattleRecord.cs
+++ HeavenlyWind.Game/Services/Records/BattleRecord.cs
@@ -20,10 +20,12 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Services.Records
                 "api_req_sortie/battle",
                 "api_req_battle_midnight/sp_midnight",
                 "api_req_sortie/airbattle",
+                "api_req_sortie/ld_airbattle",
                 "api_req_combined_battle/airbattle",
                 "api_req_combined_battle/battle",
                 "api_req_combined_battle/battle_water",
                 "api_req_combined_battle/sp_midnight",
+                "api_req_combined_battle/ld_airbattle",
                 "api_req_battle_midnight/battle",
                 "api_req_combined_battle/midnight_battle",
             };
