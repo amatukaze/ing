ffd6837c86a38e9b945e78c9265ebe1c45251550
神樹桜乃 <kodama@sakuno.moe>
2016-02-12 20:09:10 +0800

    Fix exception from aerial attack

--- HeavenlyWind.Game/Models/Raw/Battle/RawCombinedFleetAerialCombat.cs
+++ HeavenlyWind.Game/Models/Raw/Battle/RawCombinedFleetAerialCombat.cs
@@ -2,7 +2,7 @@
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models.Raw.Battle
 {
-    public class RawCombinedFleetAerialCombat : RawCombinedFleetDay
+    public class RawCombinedFleetAerialCombat : RawCombinedFleetDay, IAerialCombatSecondRound
     {
         [JsonProperty("api_kouku2")]
         public RawAerialCombatPhase AerialCombatSecondRound { get; set; }
