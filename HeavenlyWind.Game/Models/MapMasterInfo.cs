7709a68f9ebe9872f828e70388e471859b552d8b
神樹桜乃 <kodama@sakuno.moe>
2016-03-19 09:19:51 +0800

    Add a new property

--- HeavenlyWind.Game/Models/MapMasterInfo.cs
+++ HeavenlyWind.Game/Models/MapMasterInfo.cs
@@ -2,7 +2,7 @@
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models
 {
-    public class MapMasterInfo : RawDataWrapper<RawMapMasterInfo>, IID, IMapMasterInfo
+    public class MapMasterInfo : RawDataWrapper<RawMapMasterInfo>, IID, ITranslatedName, IMapMasterInfo
     {
         public int ID => RawData.ID;
 
@@ -10,22 +10,13 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models
         public int AreaSubID => RawData.MapNo;
 
         public string Name => RawData.Name;
+        public string TranslatedName => StringResources.Instance.Extra?.GetMapName(ID) ?? Name;
 
         public int? RequiredDefeatCount => RawData.RequiredDefeatCount;
 
         public CombinedFleetType SortieFleetType => (CombinedFleetType)RawData.SortieFleetType[1];
 
-        internal MapMasterInfo(RawMapMasterInfo rpRawData) : base(rpRawData)
-        {
-            OnRawDataUpdated();
-        }
-
-        protected override void OnRawDataUpdated()
-        {
-            var rTranslatedName = StringResources.Instance.Extra?.GetMapName(ID);
-            if (rTranslatedName != null)
-                RawData.Name = rTranslatedName;
-        }
+        internal MapMasterInfo(RawMapMasterInfo rpRawData) : base(rpRawData) { }
 
         public override string ToString() => $"ID = {ID}, Name = \"{Name}\"";
     }
