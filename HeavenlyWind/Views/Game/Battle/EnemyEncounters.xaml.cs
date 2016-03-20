263cf79d10e4cd4b1c1e60d2c851a28e589e8e5d
神樹桜乃 <kodama@sakuno.moe>
2016-03-20 15:16:14 +0800

    Modification on battle info

---
+++ HeavenlyWind/Views/Game/Battle/EnemyEncounters.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sakuno.KanColle.Amatsukaze.Views.Game.Battle
+{
+    /// <summary>
+    /// EnemyEncounters.xaml の相互作用ロジック
+    /// </summary>
+    partial class EnemyEncounters : UserControl
+    {
+        public EnemyEncounters()
+        {
+            InitializeComponent();
+        }
+    }
+}
