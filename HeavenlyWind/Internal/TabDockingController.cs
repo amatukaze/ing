d9622896a647e6d63d7b4e80569fdd50b4b3e994
神樹桜乃 <kodama@sakuno.moe>
2016-03-25 23:28:03 +0800

    Use the new tab control instead

---
+++ HeavenlyWind/Internal/TabDockingController.cs
@@ -0,0 +1,18 @@
+﻿using Sakuno.UserInterface.Controls;
+using Sakuno.UserInterface.Controls.Docking;
+
+namespace Sakuno.KanColle.Amatsukaze.Internal
+{
+    class TabDockingController : ITabDockingController
+    {
+        public AdvancedTabControl CreateHost(AdvancedTabControl rpSourceTabControl, string rpSourcePartition)
+        {
+            return new AdvancedTabControl()
+            {
+                DataContext = rpSourceTabControl.DataContext,
+                ItemTemplate = rpSourceTabControl.ItemTemplate,
+                TabController = new TabController() { TearOffController = rpSourceTabControl.TabController.TearOffController, Partition = rpSourceTabControl.TabController.Partition },
+            };
+        }
+    }
+}
--- HeavenlyWind/Views/MainWindow.xaml.cs
+++ HeavenlyWind/Views/MainWindow.xaml.cs
@@ -1,12 +1,8 @@
-﻿using Sakuno.KanColle.Amatsukaze.Services;
-using Sakuno.SystemInterop;
-using Sakuno.UserInterface;
+﻿using Sakuno.SystemInterop;
 using Sakuno.UserInterface.Controls;
 using System;
 using System.ComponentModel;
-using System.Reactive.Linq;
 using System.Windows;
-using System.Windows.Controls;
 
 namespace Sakuno.KanColle.Amatsukaze.Views
 {
@@ -15,23 +11,9 @@ namespace Sakuno.KanColle.Amatsukaze.Views
     /// </summary>
     public partial class MainWindow : MetroWindow
     {
-        static readonly Size r_GameInformationDefaultSize = new Size(500, 400);
-
-        Dock CurrentDock => ScreenOrientation == ScreenOrientation.Landscape ? Preference.Current.Layout.LandscapeDock : Preference.Current.Layout.PortraitDock;
-
         public MainWindow()
         {
             InitializeComponent();
-
-            if (!BrowserService.Instance.NoInstalledLayoutEngines)
-            {
-                var rLayoutPreferencePropertyChangedSource = Observable.FromEventPattern<PropertyChangedEventArgs>(Preference.Current.Layout, nameof(Preference.Current.Layout.PropertyChanged))
-                    .Select(r => r.EventArgs.PropertyName);
-                rLayoutPreferencePropertyChangedSource.Where(r => r == nameof(Preference.Current.Layout.LandscapeDock)).Subscribe(_ => UpdateSize(Preference.Current.Layout.LandscapeDock));
-                rLayoutPreferencePropertyChangedSource.Where(r => r == nameof(Preference.Current.Layout.PortraitDock)).Subscribe(_ => UpdateSize(Preference.Current.Layout.PortraitDock));
-
-                r_Browser.SizeChanged += Browser_SizeChanged;
-            }
         }
         protected override void OnSourceInitialized(EventArgs e)
         {
@@ -50,41 +32,6 @@ namespace Sakuno.KanColle.Amatsukaze.Views
             }
 
             base.OnClosing(e);
-        }
-
-        void Browser_SizeChanged(object sender, SizeChangedEventArgs e)
-        {
-            if (!(r_Browser.Host?.IsExtracted).GetValueOrDefault())
-                return;
-
-            var rDock = CurrentDock;
-            var rSize = r_Browser.DesiredSize;
-
-            MinWidth = rSize.Width;
-            MinHeight = r_CaptionBar.ActualHeight + rSize.Height + r_StatusBar.ActualHeight;
-
-            if (Width < MinWidth)
-                Width = MinWidth;
-            if (Height < MinHeight)
-                Height = MinHeight;
-
-            UpdateSize();
-        }
-
-        void UpdateSize() => UpdateSize(CurrentDock);
-        void UpdateSize(Dock rpDock)
-        {
-            var rBrowserDesiredSize = r_Browser.DesiredSize;
-            var rWidth = rBrowserDesiredSize.Width;
-            var rHeight = r_CaptionBar.ActualHeight + rBrowserDesiredSize.Height + r_StatusBar.ActualHeight;
-
-            if (rpDock == Dock.Left || rpDock == Dock.Right)
-                rWidth += Math.Max(r_Content.ActualWidth, r_GameInformationDefaultSize.Width);
-            else
-                rHeight += Math.Max(r_Content.ActualHeight, r_GameInformationDefaultSize.Height);
-
-            Width = rWidth;
-            Height = rHeight;
         }
     }
 }
