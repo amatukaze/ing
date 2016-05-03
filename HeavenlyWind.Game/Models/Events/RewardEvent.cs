73593368bc57e144850136cf54d2ee2c81a9c653
神樹桜乃 <kodama@sakuno.moe>
2016-05-03 15:24:31 +0800

    Fix exception from reward event

--- HeavenlyWind.Game/Models/Events/RewardEvent.cs
+++ HeavenlyWind.Game/Models/Events/RewardEvent.cs
@@ -1,13 +1,19 @@
 ﻿using Sakuno.KanColle.Amatsukaze.Game.Models.Raw;
+using System.Collections.Generic;
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models.Events
 {
     public class RewardEvent : RewardEventBase
     {
-        public override int TypeID => RawData.Reward.TypeID;
-        public override MaterialType ID => RawData.Reward.ID;
-        public override int Quantity => RawData.Reward.Quantity;
+        public override int TypeID => RawData.Rewards[0].TypeID;
+        public override MaterialType ID => RawData.Rewards[0].ID;
+        public override int Quantity => RawData.Rewards[0].Quantity;
 
-        internal RewardEvent(RawMapExploration rpData) : base(rpData) { }
+        public IList<RawMapExploration.RawReward> Rewards { get; }
+
+        internal RewardEvent(RawMapExploration rpData) : base(rpData)
+        {
+            Rewards = RawData.Rewards.AsReadOnly();
+        }
     }
 }
