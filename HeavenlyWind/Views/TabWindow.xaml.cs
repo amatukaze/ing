a4431ada91ed8beaa6aa56dfbb8fb1c6497f9f0f
神樹桜乃 <kodama@sakuno.moe>
2016-05-17 11:05:11 +0800

    Disable minimize function of tab window

--- HeavenlyWind/Views/TabWindow.xaml.cs
+++ HeavenlyWind/Views/TabWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using Sakuno.SystemInterop;
 using System;
+using System.Windows.Interop;
 
 namespace Sakuno.KanColle.Amatsukaze.Views
 {
@@ -16,6 +17,11 @@ namespace Sakuno.KanColle.Amatsukaze.Views
         protected override void OnSourceInitialized(EventArgs e)
         {
             base.OnSourceInitialized(e);
+
+            var rHandle = new WindowInteropHelper(this).Handle;
+
+            var rSystemMenu = NativeMethods.User32.GetSystemMenu(rHandle, false);
+            NativeMethods.User32.EnableMenuItem(rSystemMenu, NativeConstants.SystemCommand.SC_MINIMIZE, NativeEnums.MF.MF_GRAYED);
 
             NativeStructs.POINT rCursorPoint;
             NativeMethods.User32.GetCursorPos(out rCursorPoint);
