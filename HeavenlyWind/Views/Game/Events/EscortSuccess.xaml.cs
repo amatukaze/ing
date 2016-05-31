5efb5a30a9fd79e59c67cb597d47d9f006029c20
神樹桜乃 <kodama@sakuno.moe>
2016-05-31 23:06:19 +0800

    Fix the vanish of escort success event

---
+++ HeavenlyWind/Views/Game/Events/EscortSuccess.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace Sakuno.KanColle.Amatsukaze.Views.Game.Events
+{
+    /// <summary>
+    /// EscortSuccess.xaml の相互作用ロジック
+    /// </summary>
+    partial class EscortSuccess
+    {
+        public EscortSuccess()
+        {
+            InitializeComponent();
+        }
+    }
+}
