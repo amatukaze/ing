b43b5f544db64fd63f244388a21a8d1baf8af31a
神樹桜乃 <kodama@sakuno.moe>
2016-06-07 23:09:10 +0800

    Add screenshot folder picker

--- HeavenlyWind.Base/Models/Preferences/ScreenshotPreference.cs
+++ HeavenlyWind.Base/Models/Preferences/ScreenshotPreference.cs
@@ -2,10 +2,19 @@
 
 namespace Sakuno.KanColle.Amatsukaze.Models.Preferences
 {
-    public class ScreenshotPreference
+    public class ScreenshotPreference : ModelBase
     {
+        string r_Destination = "Screenshot";
         [JsonProperty("destination")]
-        public string Destination { get; set; } = "Screenshot";
+        public string Destination
+        {
+            get { return r_Destination; }
+            set
+            {
+                r_Destination = value;
+                OnPropertyChanged(nameof(Destination));
+            }
+        }
         [JsonProperty("filenameformat")]
         public string FilenameFormat { get; set; } = "ing_{0:yyyyMMdd-HHmmss-ff}.{1}";
         [JsonProperty("imageformat")]
--- HeavenlyWind/ViewModels/Preferences/PreferencesWindowViewModel.cs
+++ HeavenlyWind/ViewModels/Preferences/PreferencesWindowViewModel.cs
@@ -1,8 +1,12 @@
 ﻿using Sakuno.KanColle.Amatsukaze.Models;
 using Sakuno.KanColle.Amatsukaze.Services;
 using Sakuno.KanColle.Amatsukaze.ViewModels.Plugins;
+using Sakuno.SystemInterop.Dialogs;
+using Sakuno.UserInterface;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Windows.Input;
 using System.Windows.Media;
 
 namespace Sakuno.KanColle.Amatsukaze.ViewModels.Preferences
@@ -17,6 +21,8 @@ namespace Sakuno.KanColle.Amatsukaze.ViewModels.Preferences
 
         public bool IsAutoRotationSupported => CurrentDockExtension.IsAutoRotationSupported;
 
+        public ICommand OpenScreenshotFolderPickerCommand { get; }
+
         PreferencesWindowViewModel()
         {
             var rSystemFonts = Fonts.SystemFontFamilies.Select(r => new SystemFont(r)).ToList();
@@ -27,6 +33,17 @@ namespace Sakuno.KanColle.Amatsukaze.ViewModels.Preferences
             SystemFonts = rSystemFonts.AsReadOnly();
 
             LoadedPlugins = PluginService.Instance.LoadedPlugins.Select(r => new PluginViewModel(r)).ToList().AsReadOnly();
+
+            OpenScreenshotFolderPickerCommand = new DelegatedCommand(() => GetFilenameWithCommonFolderPicker(r => Preference.Current.Browser.Screenshot.Destination = r));
+        }
+
+        void GetFilenameWithCommonFolderPicker(Action<string> rpContinuation)
+        {
+            using (var rFolderPicker = new CommonOpenFileDialog() { FolderPicker = true })
+            {
+                if (rFolderPicker.Show() == CommonFileDialogResult.OK)
+                    rpContinuation(rFolderPicker.Filename);
+            }
         }
     }
 }
