9888ca47b14dc84d167784e44d6abecac693e126
神樹桜乃 <kodama@sakuno.moe>
2016-07-11 23:33:41 +0800

    Record admiral's position and score from ranking list

--- HeavenlyWind.Game/Models/AdmiralRankingPointsDifference.cs
+++ HeavenlyWind.Game/Models/AdmiralRankingPointsDifference.cs
@@ -33,11 +33,7 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models
                 switch (Type)
                 {
                     case AdmiralRankingPointsDifferenceType.PreviousUpdate:
-                        var rNow = DateTimeOffset.Now.ToOffset(TimeSpan.FromHours(9.0));
-                        if (rNow.Hour >= 2 && rNow.Hour < 14)
-                            rCommand.CommandText = "SELECT coalesce((SELECT max(experience) FROM admiral_experience WHERE time < strftime('%s', 'now', 'start of day', '-7 hour')), (SELECT min(experience) FROM admiral_experience WHERE time >= strftime('%s', 'now', 'start of day', '-7 hour')), @current_exp);";
-                        else
-                            rCommand.CommandText = "SELECT coalesce((SELECT max(experience) FROM admiral_experience WHERE time < strftime('%s', 'now', 'start of day', '+5 hour')), (SELECT min(experience) FROM admiral_experience WHERE time >= strftime('%s', 'now', 'start of day', '+5 hour')), @current_exp);";
+                        rCommand.CommandText = "SELECT coalesce((SELECT max(experience) FROM admiral_experience WHERE time < (CASE (strftime('%H', 'now') + 7) / 12 WHEN 1 THEN strftime('%s', 'now', 'start of day', '+5 hour') ELSE strftime('%s', 'now', 'start of day', '-7 hour') END)), (SELECT min(experience) FROM admiral_experience WHERE time >= (CASE (strftime('%H', 'now') + 7) / 12 WHEN 1 THEN strftime('%s', 'now', 'start of day', '+5 hour') ELSE strftime('%s', 'now', 'start of day', '-7 hour') END)), @current_exp);";
                         break;
 
                     case AdmiralRankingPointsDifferenceType.Day:
