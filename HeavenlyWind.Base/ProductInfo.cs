527a612e6f052f942fa8a6433ab62be4e346919e
神樹桜乃 <kodama@sakuno.moe>
2016-07-14 20:40:14 +0800

    Version 0.1.11 (Kraken)

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -5,10 +5,10 @@
         public const string AppName = "いんてりじぇんと連装砲くん";
         public const string ProductName = "Intelligent Naval Gun";
 
-        public const string AssemblyVersionString = "0.1.10.6";
+        public const string AssemblyVersionString = "0.1.11";
 
         public static string Version => AssemblyVersionString;
-        public static string ReleaseCodeName => "Tiramisu";
+        public static string ReleaseCodeName => "Kraken";
 
         public const string UserAgent = "ING/" + AssemblyVersionString;
     }
