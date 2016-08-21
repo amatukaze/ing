7649f6a6ef7ebc2c6dad2def1e186b6af72b9d15
神樹桜乃 <kodama@sakuno.moe>
2016-08-22 00:01:48 +0800

    Version 0.1.12 (Hopespeak)

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -5,10 +5,10 @@
         public const string AppName = "いんてりじぇんと連装砲くん";
         public const string ProductName = "Intelligent Naval Gun";
 
-        public const string AssemblyVersionString = "0.1.11.4";
+        public const string AssemblyVersionString = "0.1.12";
 
         public static string Version => AssemblyVersionString;
-        public static string ReleaseCodeName => "Kraken";
+        public static string ReleaseCodeName => "Hopespeak";
 
         public const string UserAgent = "ING/" + AssemblyVersionString;
     }
