28ee57ecb5448162fde74789eaabc180292f608c
神樹桜乃 <kodama@sakuno.moe>
2016-09-17 16:50:15 +0800

    Foundation of the new data source

---
+++ HeavenlyWind/Internal/ItemsFilter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sakuno.KanColle.Amatsukaze.Internal
+{
+    abstract class ItemsFilter<T> : DisposableModelBase
+    {
+        Action r_UpdateAction;
+
+        protected ItemsFilter(Action rpUpdateAction)
+        {
+            r_UpdateAction = rpUpdateAction;
+        }
+
+        public abstract bool Predicate(T rpItem);
+
+        protected void Update() => r_UpdateAction?.Invoke();
+
+        protected override void DisposeManagedResources() => r_UpdateAction = null;
+    }
+}
