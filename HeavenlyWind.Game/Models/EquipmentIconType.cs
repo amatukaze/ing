f47d41e11ed37186dd49fccb45a5143480dadf6b
神樹桜乃 <kodama@sakuno.moe>
2016-12-09 00:32:52 +0800

    Add two jet-powered aircraft icons

--- HeavenlyWind.Game/Models/EquipmentIconType.cs
+++ HeavenlyWind.Game/Models/EquipmentIconType.cs
@@ -41,5 +41,7 @@
         SpecialAmphibiousLandingCraft,
         LandBasedAttackAircraft,
         InterceptorFighter,
+        JetFighterBomberKikkaKai,
+        JetFighterBomberKeiunKai,
     }
 }
