99bf9e2509b3824353002ce16a768ed4c7cf0005
神樹桜乃 <kodama@sakuno.moe>
2017-01-15 22:13:11 +0800

    Move port customization to "Other" page

--- HeavenlyWind/Views/Preferences/Other.xaml.cs
+++ HeavenlyWind/Views/Preferences/Other.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Windows.Input;
+﻿using Sakuno.SystemInterop;
+using Sakuno.UserInterface;
+using System.Windows;
+using System.Windows.Input;
 
 namespace Sakuno.KanColle.Amatsukaze.Views.Preferences
 {
@@ -13,6 +16,39 @@ namespace Sakuno.KanColle.Amatsukaze.Views.Preferences
         public Other()
         {
             InitializeComponent();
+
+            Loaded += Other_Loaded;
+        }
+
+        void Other_Loaded(object sender, RoutedEventArgs e)
+        {
+            EnablePortCustomization.Checked += EnablePortCustomization_Checked;
+        }
+
+        void EnablePortCustomization_Checked(object sender, RoutedEventArgs e)
+        {
+            if (!EnablePortCustomization.IsChecked.Value)
+                return;
+
+            var rDialog = new TaskDialog()
+            {
+                Caption = StringResources.Instance.Main.Product_Name,
+                Instruction = StringResources.Instance.Main.PreferenceWindow_Network_Port_Customization_Dialog_Instruction,
+                Icon = TaskDialogIcon.Information,
+                Content = StringResources.Instance.Main.PreferenceWindow_Network_Port_Customization_Dialog_Content,
+                Buttons =
+                {
+                    new TaskDialogButton(TaskDialogCommonButton.Yes, StringResources.Instance.Main.PreferenceWindow_Network_Port_Customization_Dialog_Button_Yes),
+                    new TaskDialogButton(TaskDialogCommonButton.No, StringResources.Instance.Main.PreferenceWindow_Network_Port_Customization_Dialog_Button_No),
+                },
+                DefaultCommonButton = TaskDialogCommonButton.No,
+
+                OwnerWindow = WindowUtil.GetTopWindow(),
+                ShowAtTheCenterOfOwner = true,
+            };
+
+            if (rDialog.Show().ClickedCommonButton == TaskDialogCommonButton.No)
+                EnablePortCustomization.IsChecked = false;
         }
 
         void PanicKey_PreviewKeyDown(object sender, KeyEventArgs e)
