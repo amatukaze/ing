1c5c2a4b8ce486f24c63b8c1fe2132068d6a9888
神樹桜乃 <kodama@sakuno.moe>
2017-10-14 16:53:29 +0800

    The most simple code to load bootstrap

--- src/HeavenlyWind.Launcher/Program.cs
+++ src/HeavenlyWind.Launcher/Program.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +10,24 @@ namespace HeavenlyWind
 {
     static partial class Program
     {
+        const string ModulesDirectoryName = "Modules";
+
+        const string BootstrapAssemblyName = "HeavenlyWind.Bootstrap";
+        const string BootstrapTypeName = "HeavenlyWind.Bootstrap.Bootstraper";
+        const string BootstrapStartupMethodName = "Startup";
+
         static void Main(string[] args)
         {
             _defaultConsoleColor = Console.ForegroundColor;
+
+            var currentDirectory = Path.GetDirectoryName(Assembly.GetEntryAssembly().Location);
+
+            var bootstrapFilename = Path.Combine(currentDirectory, ModulesDirectoryName, BootstrapAssemblyName, BootstrapAssemblyName + ".dll");
+            var bootstrapAssembly = Assembly.LoadFile(bootstrapFilename);
+            var bootstrapType = bootstrapAssembly.GetType(BootstrapTypeName);
+            var startupMethod = bootstrapType.GetMethod(BootstrapStartupMethodName, BindingFlags.Public | BindingFlags.Static);
+
+            startupMethod.Invoke(null, null);
         }
     }
 }
