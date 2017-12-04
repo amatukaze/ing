e83f011a74a3708285269ce58c2123d4ae2c9d4e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2017-12-04 21:47:28 +0800

    Add shim package generation

---
+++ src/HeavenlyWind.BclShimSet.Fake/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace HeavenlyWind.BclShimSet.Fake
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            throw new Exception("This is a fake exe to call toolchain from SDK.");
+        }
+    }
+}
