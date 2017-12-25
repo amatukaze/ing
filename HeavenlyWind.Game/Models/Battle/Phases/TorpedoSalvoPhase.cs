8333a4a7d9d7ffc52eb1f7f9251cd5ca11aa412f
神樹桜乃 <kodama@sakuno.moe>
2017-12-25 17:25:49 +0800

    Fix the calculation of torpedo salvo phase

--- HeavenlyWind.Game/Models/Battle/Phases/TorpedoSalvoPhase.cs
+++ HeavenlyWind.Game/Models/Battle/Phases/TorpedoSalvoPhase.cs
@@ -27,59 +27,16 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Phases
             if (RawData == null)
                 return;
 
-            var rParticipants = Stage.FriendAndEnemy;
-
-            var count = RawData.FriendDamage.Length == 7 ? 7 : 6;
-
-            var rFriendDamages = RawData.FriendDamage;
-            var rFriendMainDamages = rFriendDamages.Take(count);
-            var rFriendEscortDamages = rFriendDamages.Skip(count);
-            var rEnemyDamages = RawData.EnemyDamage;
-            var rEnemyMainDamages = rEnemyDamages.Take(count);
-            var rEnemyEscortDamages = rEnemyDamages.Skip(count);
-
-            if (count == 6)
+            for (var i = 0; i < Stage.Friend.Count;i++)
             {
-                var pad = Enumerable.Repeat(-1, 1);
+                var participant = Stage.Friend[i];
 
-                rFriendMainDamages = rFriendMainDamages.Concat(pad);
-                rFriendEscortDamages = rFriendEscortDamages.Concat(pad);
-                rEnemyMainDamages = rEnemyMainDamages.Concat(pad);
-                rEnemyEscortDamages = rEnemyEscortDamages.Concat(pad);
+                participant.Current -= RawData.FriendDamage[i];
+                participant.DamageGivenToOpponent += RawData.FriendDamageGivenToOpponent[i];
             }
 
-            var rDamages = Enumerable.Concat(rFriendMainDamages, rEnemyMainDamages).Concat(Enumerable.Concat(rFriendEscortDamages, rEnemyEscortDamages)).ToArray();
-            for (var i = 0; i < rDamages.Length; i++)
-            {
-                var rParticipant = rParticipants[GetIndex(i)];
-                if (rParticipant != null)
-                    rParticipant.Current -= rDamages[i];
-            }
-
-            var rFriendDamagesGivenToOpponent = RawData.FriendDamageGivenToOpponent;
-            var rFriendMainDamagesGivenToOpponent = rFriendDamagesGivenToOpponent.Take(count);
-            var rFriendEscortDamagesGivenToOpponent = rFriendDamagesGivenToOpponent.Skip(count);
-            var rEnemyDamagesGivenToOpponent = RawData.EnemyDamageGivenToOpponent;
-            var rEnemyMainDamagesGivenToOpponent = rEnemyDamagesGivenToOpponent.Take(count);
-            var rEnemyEscortDamagesGivenToOpponent = rEnemyDamagesGivenToOpponent.Skip(count);
-
-            if (count == 6)
-            {
-                var pad = Enumerable.Repeat(-1, 1);
-
-                rFriendMainDamagesGivenToOpponent = rFriendMainDamagesGivenToOpponent.Concat(pad);
-                rFriendEscortDamagesGivenToOpponent = rFriendEscortDamagesGivenToOpponent.Concat(pad);
-                rEnemyMainDamagesGivenToOpponent = rEnemyMainDamagesGivenToOpponent.Concat(pad);
-                rEnemyEscortDamagesGivenToOpponent = rEnemyEscortDamagesGivenToOpponent.Concat(pad);
-            }
-
-            var rDamagesGivenToOpponent = Enumerable.Concat(rFriendMainDamagesGivenToOpponent, rEnemyMainDamagesGivenToOpponent).Concat(Enumerable.Concat(rFriendEscortDamagesGivenToOpponent, rEnemyEscortDamagesGivenToOpponent)).ToArray();
-            for (var i = 0; i < rDamagesGivenToOpponent.Length; i++)
-            {
-                var rParticipant = rParticipants[GetIndex(i)];
-                if (rParticipant != null)
-                    rParticipant.DamageGivenToOpponent += rDamagesGivenToOpponent[i];
-            }
+            for (var i = 0; i < Stage.Enemy.Count; i++)
+                Stage.Enemy[i].Current -= RawData.EnemyDamage[i];
         }
     }
 }
