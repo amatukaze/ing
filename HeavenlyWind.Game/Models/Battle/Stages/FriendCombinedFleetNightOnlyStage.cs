c965f2ac87e7f5a582337d2f433c53400b8b20b1
神樹桜乃 <kodama@sakuno.moe>
2017-12-28 18:48:34 +0800

    Something awkward

--- HeavenlyWind.Game/Models/Battle/Stages/FriendCombinedFleetNightOnlyStage.cs
+++ HeavenlyWind.Game/Models/Battle/Stages/FriendCombinedFleetNightOnlyStage.cs
@@ -1,16 +1,27 @@
 ﻿using Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Phases;
 using Sakuno.KanColle.Amatsukaze.Game.Models.Raw.Battle;
 using Sakuno.KanColle.Amatsukaze.Game.Parsers;
+using System.Collections.Generic;
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Stages
 {
     class FriendCombinedFleetNightOnlyStage : CombinedFleetNight
     {
+        public SupportingFirePhase SupportingFire { get; protected set; }
+
+        public override IList<BattlePhase> Phases => new BattlePhase[]
+        {
+            SupportingFire,
+
+            Shelling,
+        };
+
         internal protected FriendCombinedFleetNightOnlyStage(BattleInfo rpOwner, ApiInfo rpInfo) : base(rpOwner)
         {
             var rRawData = rpInfo.Data as RawCombinedFleetNightOnly;
 
             Shelling = new ShellingPhase(this, rRawData.Shelling, true);
+            SupportingFire = new SupportingFirePhase(this, rRawData.SupportingFire);
         }
     }
 }
