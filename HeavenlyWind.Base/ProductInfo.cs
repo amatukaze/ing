ff293f26d143cca7c9f8315d20a2d08ba9d8792c
神樹桜乃 <kodama@sakuno.moe>
2018-02-25 15:26:56 +0800

    Version 0.1.15.8 (Braindrive)

--- HeavenlyWind.Base/ProductInfo.cs
+++ HeavenlyWind.Base/ProductInfo.cs
@@ -7,11 +7,11 @@ namespace Sakuno.KanColle.Amatsukaze
         public const string AppName = "いんてりじぇんと連装砲くん";
         public const string ProductName = "Intelligent Naval Gun";
 
-        public const string AssemblyVersionString = "0.1.15.7";
+        public const string AssemblyVersionString = "0.1.15.8";
 
         public static string Version => AssemblyVersionString;
         public static string ReleaseCodeName => "Braindrive";
-        public static string ReleaseDate => "2017.2.16";
+        public static string ReleaseDate => "2018.2.25";
 
         public const string UserAgent = "ING/" + AssemblyVersionString;
 
