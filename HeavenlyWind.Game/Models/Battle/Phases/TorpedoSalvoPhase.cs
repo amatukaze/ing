aa129dc94d9daa6de6cbc68c590981a99c721658
神樹桜乃 <kodama@sakuno.moe>
2018-02-27 01:57:13 +0800

    Fix calculation of torpedo salvo phase

--- HeavenlyWind.Game/Models/Battle/Phases/TorpedoSalvoPhase.cs
+++ HeavenlyWind.Game/Models/Battle/Phases/TorpedoSalvoPhase.cs
@@ -1,4 +1,5 @@
 ﻿using Sakuno.KanColle.Amatsukaze.Game.Models.Raw.Battle;
+using System;
 
 namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Phases
 {
@@ -13,17 +14,35 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Phases
             if (RawData == null)
                 return;
 
+            var friendMainCount = Math.Max(Stage.FriendMain.Count, 6);
+
             for (var i = 0; i < Stage.Friend.Count;i++)
             {
-                var participant = Stage.Friend[i];
+                BattleParticipantSnapshot participant;
+
+                if (i < Stage.FriendMain.Count)
+                    participant = Stage.FriendMain[i];
+                else if (i >= friendMainCount)
+                    participant = Stage.FriendEscort[i - friendMainCount];
+                else
+                    continue;
 
                 participant.Current -= RawData.FriendDamage[i];
                 participant.DamageGivenToOpponent += RawData.FriendDamageGivenToOpponent[i];
             }
 
+            var enemyMainCount = Math.Max(Stage.EnemyMain.Count, 6);
+
             for (var i = 0; i < Stage.Enemy.Count; i++)
             {
-                var participant = Stage.Enemy[i];
+                BattleParticipantSnapshot participant;
+
+                if (i < Stage.EnemyMain.Count)
+                    participant = Stage.EnemyMain[i];
+                else if (i >= enemyMainCount)
+                    participant = Stage.EnemyEscort[i - enemyMainCount];
+                else
+                    continue;
 
                 participant.Current -= RawData.EnemyDamage[i];
                 participant.DamageGivenToOpponent += RawData.EnemyDamageGivenToOpponent[i];
