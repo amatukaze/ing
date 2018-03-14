461218096dfd1a1f5e82d49280d26ff7aa7c76b1
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-14 20:33:37 +0800

    Simplify project file

--- src/HeavenlyWind.UWP.Shell/Properties/AssemblyInfo.cs
+++ src/HeavenlyWind.UWP.Shell/Properties/AssemblyInfo.cs
@@ -2,9 +2,6 @@
 
 [assembly: AssemblyTitle("HeavenlyWind.UWP.Shell")]
 [assembly: AssemblyDescription("The UWP shell of Intelligent Naval Gun")]
-[assembly: AssemblyCompany("Kodama Sakuno")]
-[assembly: AssemblyProduct("Intelligent Naval Gun")]
-[assembly: AssemblyCopyright("Copyright © 2017 Kodama Sakuno")]
 
-[assembly: AssemblyVersion("0.1")]
+[assembly: AssemblyVersion("0.1.0")]
 [assembly: AssemblyInformationalVersion("0.1.0-blueprint")]
---
+++ src/SharedAssemblyInfo.cs
@@ -0,0 +1,5 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyCompany("Kodama Sakuno")]
+[assembly: AssemblyProduct("Intelligent Naval Gun")]
+[assembly: AssemblyCopyright("Copyright © 2017-2018 Kodama Sakuno")]
