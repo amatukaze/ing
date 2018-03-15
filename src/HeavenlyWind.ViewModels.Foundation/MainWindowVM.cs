19e1ee2d75b41e71068d517f86b915deffa87d52
神樹桜乃 <kodama@sakuno.moe>
2018-03-15 17:04:56 +0800

    Add title property for MainWindow

--- src/HeavenlyWind.ViewModels.Foundation/MainWindowVM.cs
+++ src/HeavenlyWind.ViewModels.Foundation/MainWindowVM.cs
@@ -2,6 +2,18 @@
 {
     public class MainWindowVM : ManualNotifyObject
     {
-
+        string _title;
+        public string Title
+        {
+            get => _title;
+            set
+            {
+                if (_title != value)
+                {
+                    _title = value;
+                    NotifyPropertyChanged();
+                }
+            }
+        }
     }
 }
