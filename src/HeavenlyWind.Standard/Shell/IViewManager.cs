478936c4392469c1ffa29192ab46c8408cccce9e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-15 21:34:48 +0800

    View management interface

---
+++ src/HeavenlyWind.Standard/Shell/IViewManager.cs
@@ -0,0 +1,9 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.Shell
+{
+    public interface IViewManager
+    {
+        void RegisterView(ViewDescriptor descriptor);
+    }
+}
