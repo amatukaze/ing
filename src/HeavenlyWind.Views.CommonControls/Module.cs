5a3be12cf270c8b6dc7882b398b05662411f0e9e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-16 00:34:16 +0800

    Add wpf common controls project

---
+++ src/HeavenlyWind.Views.CommonControls/Module.cs
@@ -0,0 +1,12 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.Views
+{
+    internal class Module : IModule
+    {
+        public void Initialize(IResolver resolver)
+        {
+            ResolveExtension.Resolver = resolver;
+        }
+    }
+}
---
+++ src/HeavenlyWind.Views.CommonControls/ResolveExtension.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Markup;
+using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.Views
+{
+    public class ResolveExtension : MarkupExtension
+    {
+        internal static IResolver Resolver;
+
+        public ResolveExtension(Type targetType) => TargetType = targetType;
+
+        [ConstructorArgument("targetType")]
+        public Type TargetType { get; set; }
+
+        public override object ProvideValue(IServiceProvider serviceProvider) => Resolver.Resolve(TargetType);
+    }
+}
