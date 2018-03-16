b278b3c106c8fc1dec8734583252094d813346c8
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-17 01:17:37 +0800

    Add debug member for listener

--- src/HeavenlyWind.Game.Provider/GameListener.cs
+++ src/HeavenlyWind.Game.Provider/GameListener.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 using Sakuno.KanColle.Amatsukaze.Messaging;
 using Sakuno.KanColle.Amatsukaze.Services;
 
@@ -14,12 +16,28 @@ namespace Sakuno.KanColle.Amatsukaze.Game
             MissingMemberHandling = MissingMemberHandling.Ignore
         };
 
-        public GameListener(ITextStreamProvider provider) => this.provider = provider;
+        public GameListener(ITextStreamProvider provider)
+        {
+            this.provider = provider;
+            jSerializer.Error += JsonError;
+        }
 
-        private T Convert<T>(TextReader reader) => jSerializer.Deserialize<T>(new JsonTextReader(reader));
+        private void JsonError(object sender, Newtonsoft.Json.Serialization.ErrorEventArgs e)
+        {
+            //Debug usage
+        }
+
+        private T Convert<T>(Stream stream)
+        {
+            stream.Seek(0, SeekOrigin.Begin);
+            return jSerializer.Deserialize<T>(new JsonTextReader(new StreamReader(stream)));
+        }
 
         public IProducer<T> RegisterRaw<T>(string api) => provider
             .Where(arg => arg.Key.Equals(api, StringComparison.Ordinal))
             .Select(arg => Convert<T>(arg.Value));
+
+        public IProducer<KeyValuePair<string, JToken>> RegisterAny() => provider
+            .Select(arg => new KeyValuePair<string, JToken>(arg.Key, Convert<JToken>(arg.Value)));
     }
 }
