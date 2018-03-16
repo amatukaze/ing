0ffda09ab5e28f0c7ce2f843903cf6d60ca7ca42
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-16 23:33:28 +0800

    Game message listening

---
+++ src/HeavenlyWind.Game.Provider/GameListener.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.IO;
+using Newtonsoft.Json;
+using Sakuno.KanColle.Amatsukaze.Messaging;
+using Sakuno.KanColle.Amatsukaze.Services;
+
+namespace Sakuno.KanColle.Amatsukaze.Game
+{
+    public sealed partial class GameListener
+    {
+        private ITextStreamProvider provider;
+        private JsonSerializer jSerializer = new JsonSerializer
+        {
+            MissingMemberHandling = MissingMemberHandling.Ignore
+        };
+
+        public GameListener(ITextStreamProvider provider) => this.provider = provider;
+
+        private T Convert<T>(TextReader reader) => jSerializer.Deserialize<T>(new JsonTextReader(reader));
+
+        public IProducer<T> RegisterRaw<T>(string api) => provider
+            .Where(arg => arg.Key.Equals(api, StringComparison.Ordinal))
+            .Select(arg => Convert<T>(arg.Value));
+    }
+}
---
+++ src/HeavenlyWind.Game.Provider/Module.cs
@@ -0,0 +1,13 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.Game
+{
+    internal class Module : IExposableModule
+    {
+        public void Expose(IBuilder builder)
+        {
+            builder.RegisterType<GameListener>();
+        }
+        public void Initialize(IResolver resolver) { }
+    }
+}
