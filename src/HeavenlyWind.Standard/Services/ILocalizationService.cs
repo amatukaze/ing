3d579af00e1bc94143be0592adc1fd9c7b4d3272
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-16 16:08:00 +0800

    Add localization service

---
+++ src/HeavenlyWind.Standard/Services/ILocalizationService.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Globalization;
+
+namespace Sakuno.KanColle.Amatsukaze.Services
+{
+    public interface ILocalizationService
+    {
+        CultureInfo CurrentCulture { get; set; }
+
+        string GetLocalized(string category, string id);
+
+        event Action CultureChanged;
+    }
+}
