1fead4da9100e5f0e5e2a9f9b6385ce377bd568c
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-21 01:53:09 +0800

    Sqlite data provider

---
+++ src/HeavenlyWind.UWP.Data/DataService.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sakuno.KanColle.Amatsukaze.Data;
+using Windows.Storage;
+using Windows.Storage.AccessCache;
+
+namespace Sakuno.KanColle.Amatsukaze.UWP.Data
+{
+    internal class DataService : IDataService
+    {
+        private StorageFile dataFile;
+        internal void Initialize()
+        {
+            if (dataFile != null) return;
+            InitializeAsync().Wait();
+        }
+
+        private async Task InitializeAsync()
+        {
+            StorageItemAccessList futureList = StorageApplicationPermissions.FutureAccessList;
+            StorageFolder dataRoot = null;
+
+            foreach (var entry in futureList.Entries)
+                if (entry.Metadata == "data")
+                {
+                    dataRoot = await futureList.GetFolderAsync(entry.Token);
+                    break;
+                }
+            if (dataRoot == null)
+                dataRoot = ApplicationData.Current.RoamingFolder;
+            dataFile = await dataRoot.CreateFileAsync("ing.db", CreationCollisionOption.OpenIfExists);
+        }
+
+        public void Configure(DbContextOptionsBuilder builder)
+        {
+            if (dataFile == null)
+                throw new InvalidOperationException("Data service not initialized.");
+
+            builder.UseSqlite("Data Source=" + dataFile.Path);
+        }
+    }
+}
---
+++ src/HeavenlyWind.UWP.Data/Module.cs
@@ -0,0 +1,18 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+using Sakuno.KanColle.Amatsukaze.Data;
+
+namespace Sakuno.KanColle.Amatsukaze.UWP.Data
+{
+    internal class Module : IExposableModule
+    {
+        private DataService service = new DataService();
+        public void Expose(IBuilder builder)
+        {
+            builder.RegisterInstance<IDataService>(service);
+        }
+        public void Initialize(IResolver resolver)
+        {
+            service.Initialize();
+        }
+    }
+}
