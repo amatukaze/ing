59e412b39788b9c0599bf4e4887d199be3426ec2
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-23 13:07:32 +0800

    Move to new bootstrapper

--- src/HeavenlyWind.Launcher/BootstrapperLoader.cs
+++ src/HeavenlyWind.Launcher/BootstrapperLoader.cs
@@ -9,13 +9,15 @@ namespace Sakuno.KanColle.Amatsukaze
     {
         public static void Startup(string[] commandLine, IEnumerable<Package> packages)
         {
-            Bootstraper.Startup(commandLine, packages.Select(p => new PackageStartupInfo
+            Bootstraper.Initialize(commandLine, packages.Select(p => new PackageStartupInfo
             {
                 Id = p.Id,
                 Version = p.Version,
                 Module = p.IsModulePackage ? p.MainAssembly?.LazyAssembly : null,
                 Dependencies = p.Dependencies.ToDictionary(d => d.Id, d => d.Version, StringComparer.OrdinalIgnoreCase)
             }), new PackageStorage(ManifestUtil.TargetFrameworks));
+
+            Bootstraper.Startup();
         }
     }
 }
