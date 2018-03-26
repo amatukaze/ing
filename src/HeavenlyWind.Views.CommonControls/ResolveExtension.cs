3464f0702aa3ebb9b2ffde85f56277f0cb8f7a50
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-03-26 21:51:15 +0800

    Use static resolver

--- src/HeavenlyWind.Views.CommonControls/ResolveExtension.cs
+++ src/HeavenlyWind.Views.CommonControls/ResolveExtension.cs
@@ -6,13 +6,11 @@ namespace Sakuno.KanColle.Amatsukaze.Views
 {
     public class ResolveExtension : MarkupExtension
     {
-        internal static IResolver Resolver;
-
         public ResolveExtension(Type targetType) => TargetType = targetType;
 
         [ConstructorArgument("targetType")]
         public Type TargetType { get; set; }
 
-        public override object ProvideValue(IServiceProvider serviceProvider) => Resolver.Resolve(TargetType);
+        public override object ProvideValue(IServiceProvider serviceProvider) => StaticResolver.Instance.Resolve(TargetType);
     }
 }
