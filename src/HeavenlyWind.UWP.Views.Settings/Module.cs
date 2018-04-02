7276f4a8d86a0a45e2ea2f08477e777417e20869
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-02 22:33:21 +0800

    Add setting views project

---
+++ src/HeavenlyWind.UWP.Views.Settings/Module.cs
@@ -0,0 +1,11 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.UWP.Views.Settings
+{
+    internal class Module : IModule
+    {
+        public void Initialize(IResolver resolver)
+        {
+        }
+    }
+}
