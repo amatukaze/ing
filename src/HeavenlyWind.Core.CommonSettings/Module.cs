81942e0a5fab7b0cbac13b44361360f94390a5cf
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-02 17:08:39 +0800

    Add common settings

---
+++ src/HeavenlyWind.Core.CommonSettings/Module.cs
@@ -0,0 +1,14 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.Settings
+{
+    class Module : IModule, IExposableModule
+    {
+        public void Expose(IBuilder builder)
+        {
+            builder.RegisterType<ProxySetting>();
+        }
+
+        public void Initialize(IResolver resolver) { }
+    }
+}
---
+++ src/HeavenlyWind.Core.CommonSettings/ProxySetting.cs
@@ -0,0 +1,18 @@
+﻿namespace Sakuno.KanColle.Amatsukaze.Settings
+{
+    public class ProxySetting
+    {
+        public ProxySetting(ISettingsManager manager)
+        {
+            ListeningPort = manager.Register("proxy_listening_port", 15551);
+            UseUpstream = manager.Register("proxy_upstream_enabled", false);
+            Upstream = manager.Register("proxy_upstream", "localhost");
+            UpstreamPort = manager.Register("proxy_upstream_port", 8099);
+        }
+
+        public ISettingItem<int> ListeningPort { get; }
+        public ISettingItem<bool> UseUpstream { get; }
+        public ISettingItem<string> Upstream { get; }
+        public ISettingItem<int> UpstreamPort { get; }
+    }
+}
