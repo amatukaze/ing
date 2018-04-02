ebf61db4ccd99bef3a91bf95006ee270f4b5b6fc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-02 19:17:46 +0800

    Add settings view

---
+++ src/HeavenlyWind.UWP/SettingsView.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Sakuno.KanColle.Amatsukaze.Settings;
+using Windows.UI.Xaml.Controls;
+
+//https://go.microsoft.com/fwlink/?LinkId=234236 上介绍了“用户控件”项模板
+
+namespace Sakuno.KanColle.Amatsukaze.UWP
+{
+
+    internal class SettingsPageModel
+    {
+        public string Title; //TODO:localize
+        public object[] Content;
+    }
+
+    public sealed partial class SettingsView : UserControl
+    {
+        private SettingsPageModel[] Pages;
+        public SettingsView(List<(Type ViewType, SettingCategory Category)> entries)
+        {
+            this.InitializeComponent();
+            Pages = entries
+                .GroupBy(e => e.Category)
+                .OrderBy(e => e.Key)
+                .Select(e => new SettingsPageModel
+                {
+                    Title = e.Key.ToString(),
+                    Content = e.Select(t => Activator.CreateInstance(t.ViewType)).ToArray()
+                }).ToArray();
+        }
+    }
+}
