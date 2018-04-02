8909ad5e84f0135d7b27611e04e7d90be543dc16
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-02 18:23:14 +0800

    Add setting view

---
+++ src/HeavenlyWind.Standard/Settings/SettingCategory.cs
@@ -0,0 +1,13 @@
+﻿namespace Sakuno.KanColle.Amatsukaze.Settings
+{
+    public enum SettingCategory
+    {
+        Appearance,
+        Theme,
+        Network,
+        Browser,
+        Notification,
+        Game,
+        Misc = int.MaxValue
+    }
+}
--- src/HeavenlyWind.Standard/Shell/IShell.cs
+++ src/HeavenlyWind.Standard/Shell/IShell.cs
@@ -1,10 +1,12 @@
-﻿using Sakuno.KanColle.Amatsukaze.Composition;
+﻿using System;
+using Sakuno.KanColle.Amatsukaze.Settings;
 
 namespace Sakuno.KanColle.Amatsukaze.Shell
 {
     public interface IShell
     {
         void Run();
-        void RegisterView(ViewDescriptor descriptor);
+        void RegisterView(Type viewType, string id, bool isFixedSize = true, bool singleWindowRecommended = false);
+        void RegisterSettingView(Type viewType, SettingCategory category = SettingCategory.Misc);
     }
 }
