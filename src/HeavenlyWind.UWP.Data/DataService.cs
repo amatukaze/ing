c10d220d04fecaf92c839b04423a83201a38abc9
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-03 17:10:35 +0800

    Remove storage selection

--- src/HeavenlyWind.UWP.Data/DataService.cs
+++ src/HeavenlyWind.UWP.Data/DataService.cs
@@ -1,11 +1,9 @@
-﻿using Microsoft.EntityFrameworkCore;
-using Sakuno.KanColle.Amatsukaze.Data;
-using System;
+﻿using System;
 using System.IO;
-using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Sakuno.KanColle.Amatsukaze.Data;
 using Windows.Storage;
-using Windows.Storage.AccessCache;
 
 namespace Sakuno.KanColle.Amatsukaze.UWP.Data
 {
@@ -17,52 +15,22 @@ namespace Sakuno.KanColle.Amatsukaze.UWP.Data
         internal async Task InitializeAsync()
         {
             if (dataRoot != null) return;
-            StorageItemAccessList futureList = StorageApplicationPermissions.FutureAccessList;
-
-            foreach (var entry in futureList.Entries)
-                if (entry.Metadata == "data")
-                {
-                    dataRoot = await futureList.GetFolderAsync(entry.Token);
-                    break;
-                }
-            if (dataRoot != null)
-                dbFile = await CreateDbFileAsync();
+            dataRoot = ApplicationData.Current.RoamingFolder;
+            dbFile = await dataRoot.CreateFileAsync("ing.db", CreationCollisionOption.OpenIfExists);
         }
 
         public void ConfigureDbContext(DbContextOptionsBuilder builder)
         {
-            DelayInitializeWithUIAsync().Wait();
             if (dbFile == null)
                 throw new InvalidOperationException("Data service not initialized.");
 
             builder.UseSqlite("Data Source=" + dbFile.Path);
         }
 
-        public async Task<Stream> ReadFile(string filename)
-        {
-            await DelayInitializeWithUIAsync();
-            return await dataRoot.OpenStreamForReadAsync(filename);
-        }
+        public Task<Stream> ReadFile(string filename)
+            => dataRoot.OpenStreamForReadAsync(filename);
 
-        public async Task<Stream> WriteFile(string filename)
-        {
-            await DelayInitializeWithUIAsync();
-            return await dataRoot.OpenStreamForWriteAsync(filename, CreationCollisionOption.ReplaceExisting);
-        }
-
-        private ConfiguredTaskAwaitable<StorageFile> CreateDbFileAsync()
-            => dataRoot.CreateFileAsync("ing.db", CreationCollisionOption.OpenIfExists).AsTask().ConfigureAwait(false);
-
-        private async Task DelayInitializeWithUIAsync()
-        {
-            if (dataRoot != null) return;
-
-            var dialog = new StorageSelectionDialog();
-            await dialog.ShowAsync();
-
-            dataRoot = dialog.SelectedFolder;
-            StorageApplicationPermissions.FutureAccessList.Add(dataRoot, "data");
-            dbFile = await CreateDbFileAsync();
-        }
+        public Task<Stream> WriteFile(string filename)
+            => dataRoot.OpenStreamForWriteAsync(filename, CreationCollisionOption.ReplaceExisting);
     }
 }
