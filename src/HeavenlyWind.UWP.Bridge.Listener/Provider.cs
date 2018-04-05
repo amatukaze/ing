d377d58bd5de3aaba6a0d22154948af3e8e48322
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-05 20:45:28 +0800

    Receive bridge message

--- src/HeavenlyWind.UWP.Bridge.Listener/Provider.cs
+++ src/HeavenlyWind.UWP.Bridge.Listener/Provider.cs
@@ -1,16 +1,69 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Threading;
 using Sakuno.KanColle.Amatsukaze.Services;
+using Windows.Web.Http;
 
 namespace Sakuno.KanColle.Amatsukaze.UWP.Bridge
 {
     internal class Provider : ITextStreamProvider
     {
-        public bool Enabled { get; set; }
+        private readonly IDateTimeService dateTimeService;
+        public Provider(IDateTimeService dateTimeService) => this.dateTimeService = dateTimeService;
+
+        private bool _enable;
+        public bool Enabled
+        {
+            get => _enable;
+            set
+            {
+                if (_enable == value) return;
+                _enable = value;
+
+                if (value)
+                {
+                    cts = new CancellationTokenSource();
+                    ReceiverLoop(cts.Token);
+                }
+                else
+                {
+                    cts?.Cancel();
+                    cts = null;
+                }
+            }
+        }
 
         public event Action<TextMessage> Received;
+
+        private CancellationTokenSource cts;
+        private readonly HttpClient client = new HttpClient();
+        private async void ReceiverLoop(CancellationToken token)
+        {
+            while (!token.IsCancellationRequested)
+            {
+                try
+                {
+                    string host, path, request;
+                    MemoryStream mms;
+                    using (var stream = (await client.GetInputStreamAsync(new Uri(Constants.HttpHost))).AsStreamForRead())
+                    {
+                        var reader = new StreamReader(stream);
+                        host = await reader.ReadLineAsync();
+                        if (string.IsNullOrEmpty(host)) continue;
+                        path = await reader.ReadLineAsync();
+                        request = await reader.ReadLineAsync();
+                        var svdataBuffer = new byte[7];
+                        await stream.ReadAsync(svdataBuffer, 0, 7);
+                        mms = new MemoryStream();
+                        await stream.CopyToAsync(mms);
+                    }
+                    Received?.Invoke(new TextMessage(path, dateTimeService.Now, mms));
+                }
+                catch
+                {
+                    //
+                }
+            }
+        }
     }
 }
