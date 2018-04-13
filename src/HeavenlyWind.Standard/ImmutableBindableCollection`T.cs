f4fa7af181bd206330758a1ad3a87baf785ac773
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-14 02:41:47 +0800

    Immutable bindable collection

---
+++ src/HeavenlyWind.Standard/ImmutableBindableCollection`T.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Collections.Specialized;
+using System.ComponentModel;
+using System.Linq;
+
+namespace Sakuno.KanColle.Amatsukaze
+{
+    public sealed class ImmutableBindableCollection<T> : ReadOnlyCollection<T>, IBindableCollection<T>
+    {
+        public ImmutableBindableCollection() : base(Array.Empty<T>()) { }
+        public ImmutableBindableCollection(IList<T> source) : base(source) { }
+
+        event PropertyChangedEventHandler INotifyPropertyChanged.PropertyChanged
+        {
+            add { }
+            remove { }
+        }
+        event NotifyCollectionChangedEventHandler INotifyCollectionChanged.CollectionChanged
+        {
+            add { }
+            remove { }
+        }
+    }
+
+    public static class ImmutableCollectionExtension
+    {
+        public static ImmutableBindableCollection<T> AsBindable<T>(this IEnumerable<T> source)
+            => new ImmutableBindableCollection<T>(source as IList<T> ?? source.ToList());
+    }
+}
