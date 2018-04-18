dd8578fb4ac004882fbd060bd5118f20224eeafc
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-18 23:50:51 +0800

    Expose LocalizableTextStore

---
+++ src/HeavenlyWind.ViewModels.Foundation/Module.cs
@@ -0,0 +1,13 @@
+﻿using Sakuno.KanColle.Amatsukaze.Composition;
+
+namespace Sakuno.KanColle.Amatsukaze.ViewModels
+{
+    internal class Module : IExposableModule
+    {
+        public void Expose(IBuilder builder)
+        {
+            builder.RegisterType<LocalizableTextStore>();
+        }
+        public void Initialize(IResolver resolver) { }
+    }
+}
