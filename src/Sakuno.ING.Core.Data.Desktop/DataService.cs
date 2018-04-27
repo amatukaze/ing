af738101facefb074e4c1364350cbc9cc70813cd
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-04-27 20:27:05 +0800

    Add data service

---
+++ src/Sakuno.ING.Core.Data.Desktop/DataService.cs
@@ -0,0 +1,21 @@
+﻿using System.IO;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace Sakuno.ING.Data
+{
+    internal class DataService : IDataService
+    {
+        private readonly string basePath = Path.Combine
+        (
+            Path.GetDirectoryName(System.Reflection.Assembly.GetEntryAssembly().CodeBase),
+            "data"
+        );
+
+        public void ConfigureDbContext(DbContextOptionsBuilder builder)
+            => builder.UseSqlite($"DataSource={Path.Combine(basePath, "ing.db")}");
+
+        public Task<Stream> ReadFile(string filename) => Task.FromResult<Stream>(File.OpenRead(Path.Combine(basePath, filename)));
+        public Task<Stream> WriteFile(string filename) => Task.FromResult<Stream>(File.OpenWrite(Path.Combine(basePath, filename)));
+    }
+}
