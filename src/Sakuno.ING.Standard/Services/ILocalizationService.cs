a57622de13b22e077018654f5200a84281520921
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-05-01 00:33:28 +0800

    Add supported locales for localization service

--- src/Sakuno.ING.Standard/Services/ILocalizationService.cs
+++ src/Sakuno.ING.Standard/Services/ILocalizationService.cs
@@ -1,7 +1,11 @@
-﻿namespace Sakuno.ING.Services
+﻿using System.Collections.Generic;
+using System.Globalization;
+
+namespace Sakuno.ING.Services
 {
     public interface ILocalizationService
     {
+        IReadOnlyCollection<CultureInfo> SupportedCultures { get; }
         string GetLocalized(string category, string id);
         string GetUnlocalized(string category, string id);
     }
