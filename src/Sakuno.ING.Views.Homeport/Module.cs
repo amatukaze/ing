14039ef7b5d18a0a378bcb5f90a8661d6bf11093
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-05-01 20:46:46 +0800

    Fleets view prototype

---
+++ src/Sakuno.ING.Views.Homeport/Module.cs
@@ -0,0 +1,14 @@
+﻿using Sakuno.ING.Composition;
+using Sakuno.ING.Shell;
+
+namespace Sakuno.ING.Views.Homeport
+{
+    internal class Module : IModule
+    {
+        public void Initialize(IResolver resolver)
+        {
+            var shell = resolver.Resolve<IShell>();
+            shell.RegisterView(typeof(FleetsView), "Fleets", false);
+        }
+    }
+}
