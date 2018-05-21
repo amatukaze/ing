d02a233c47d1f4e8248ec97e831784a6475bb6ea
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-05-21 17:51:02 +0800

    Remove unnecessary members in MasterDataView

--- src/Sakuno.ING.UWP.Views.MasterData/MasterDataView.xaml.cs
+++ src/Sakuno.ING.UWP.Views.MasterData/MasterDataView.xaml.cs
@@ -1,7 +1,5 @@
 ﻿using Sakuno.ING.Composition;
-using Sakuno.ING.Game;
 using Sakuno.ING.Game.Models;
-using System.Linq;
 using Windows.UI.Xaml.Controls;
 
 namespace Sakuno.ING.UWP.Views.MasterData
@@ -13,9 +11,5 @@ namespace Sakuno.ING.UWP.Views.MasterData
         {
             this.InitializeComponent();
         }
-
-        private static IBindableCollection<T> GetDefaultView<T>(ITable<T> source)
-            where T : IIdentifiable
-            => new BindableSnapshotCollection<T>(source, source.OrderBy(x => x.Id));
     }
 }
