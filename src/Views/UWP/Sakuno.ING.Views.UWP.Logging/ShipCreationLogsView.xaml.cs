5a4c2ff8b8688565a5a4ea2490435ba71627b889
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-07-06 09:42:30 +0800

    Add logs view for UWP using DataGrid

---
+++ src/Views/UWP/Sakuno.ING.Views.UWP.Logging/ShipCreationLogsView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Sakuno.ING.Shell;
+using Sakuno.ING.ViewModels.Logging;
+using Windows.UI.Xaml.Controls;
+
+namespace Sakuno.ING.Views.UWP.Logging
+{
+    [ExportView("ShipCreationLogs")]
+    public sealed partial class ShipCreationLogsView : UserControl
+    {
+        private ShipCreationLogsVM ViewModel;
+        public ShipCreationLogsView(ShipCreationLogsVM viewModel)
+        {
+            ViewModel = viewModel;
+            this.InitializeComponent();
+        }
+    }
+}
