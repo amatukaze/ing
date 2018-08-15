3d803744345f8369167b9defa36db25629d478b2
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-08-15 23:34:57 +0800

    View for building and repairing docks

---
+++ src/Views/Desktop/Sakuno.ING.Views.Desktop.Homeport/DocksView.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Windows.Controls;
+using Sakuno.ING.Shell;
+
+namespace Sakuno.ING.Views.Desktop.Homeport
+{
+    [ExportView("Docks")]
+    public partial class DocksView : UserControl
+    {
+        public DocksView()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/DocksView.xaml.cs
@@ -0,0 +1,17 @@
+﻿using Sakuno.ING.Game.Models;
+using Sakuno.ING.Shell;
+using Windows.UI.Xaml.Controls;
+
+namespace Sakuno.ING.Views.UWP.Homeport
+{
+    [ExportView("Docks")]
+    public sealed partial class DocksView : UserControl
+    {
+        private readonly NavalBase ViewModel;
+        public DocksView(NavalBase viewModel)
+        {
+            ViewModel = viewModel;
+            this.InitializeComponent();
+        }
+    }
+}
