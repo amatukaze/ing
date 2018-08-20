ca06a80841068eb8d370d3c7467e2123dc1676fc
神樹桜乃 <kodama@sakuno.moe>
2018-08-20 19:55:26 +0800

    Little modification

--- src/HeavenlyWind/Services/Browser/BrowserWrapper.cs
+++ src/HeavenlyWind/Services/Browser/BrowserWrapper.cs
@@ -162,8 +162,6 @@ namespace Sakuno.KanColle.Amatsukaze.Services.Browser
             });
             RegisterMessageHandler(CommunicatorMessages.SetBlinkMaxFramerate, r =>
             {
-                Console.WriteLine(r);
-
                 if (r_Browser is IBlinkBrowser blinkBrowser)
                     blinkBrowser.OnMaxFramerateChanged(int.Parse(r));
             });
