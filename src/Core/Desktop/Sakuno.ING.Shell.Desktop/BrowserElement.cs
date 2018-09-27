b5b990b90e38974dbcd3a4b25252f45a965df1e7
神樹桜乃 <kodama@sakuno.moe>
2018-09-27 22:08:00 +0800

    Shutdown browser on application exit

--- src/Core/Desktop/Sakuno.ING.Shell.Desktop/BrowserElement.cs
+++ src/Core/Desktop/Sakuno.ING.Shell.Desktop/BrowserElement.cs
@@ -1,13 +1,26 @@
-﻿using System.Windows.Controls;
+﻿using System.Windows;
+using System.Windows.Controls;
+using Sakuno.ING.Browser.Desktop;
 
 namespace Sakuno.ING.Shell.Desktop
 {
     [ExportView("Browser")]
     internal class BrowserElement : ContentControl
     {
+        IBrowserProvider _browserProvider;
+
         public BrowserElement(BrowserSelector selector)
         {
-            Content = selector.SelectedBrowser.CreateBrowser();
+            _browserProvider = selector.SelectedBrowser;
+
+            Content = _browserProvider.CreateBrowser();
+
+            Application.Current.Exit += OnApplicationExit;
+        }
+
+        void OnApplicationExit(object sender, ExitEventArgs e)
+        {
+            _browserProvider.Dispose();
         }
     }
 }
