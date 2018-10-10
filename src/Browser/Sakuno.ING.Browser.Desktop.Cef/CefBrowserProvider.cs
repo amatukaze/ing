eb25675a70340a350b27bc47511beb9b98d41f73
神樹桜乃 <kodama@sakuno.moe>
2018-10-11 01:09:55 +0800

    Correct the clear cache operation

--- src/Browser/Sakuno.ING.Browser.Desktop.Cef/CefBrowserProvider.cs
+++ src/Browser/Sakuno.ING.Browser.Desktop.Cef/CefBrowserProvider.cs
@@ -5,6 +5,8 @@ using System.Windows;
 using Sakuno.CefSharp.Wpf;
 using Sakuno.ING.Composition;
 using Sakuno.ING.Http;
+using Sakuno.ING.Settings;
+using Sakuno.ING.Shell;
 using CefClass = CefSharp.Cef;
 using CefProxy = CefSharp.ProxyOptions;
 using CefSetting = CefSharp.CefSharpSettings;
@@ -17,19 +19,38 @@ namespace Sakuno.ING.Browser.Desktop.Cef
         public string Id => "Cef";
 
         private readonly IHttpProxy proxy;
+        private readonly IShellContextService shellContextService;
+
         public IHttpProvider HttpProvider => proxy;
         public bool IsSupported => true;
 
         private string cachePath;
+        private ISettingItem<bool> clearCacheOnStartup;
 
-        public CefBrowserProvider(IHttpProxy proxy)
+        public CefBrowserProvider(IHttpProxy proxy, IShellContextService shellContextService, ISettingsManager settings)
         {
             this.proxy = proxy;
+            this.shellContextService = shellContextService;
+
+            this.clearCacheOnStartup = settings.Register("cef.clear_cache_on_startup", false);
         }
 
         public void Initialize()
         {
             this.cachePath = Path.Combine(Environment.CurrentDirectory, "Browser Cache", "Cef");
+
+            if (this.clearCacheOnStartup.Value)
+            {
+                try
+                {
+                    Directory.Delete(this.cachePath, true);
+
+                    this.clearCacheOnStartup.Value = false;
+                }
+                catch
+                {
+                }
+            }
 
             CefSetting.Proxy = new CefProxy("localhost", proxy.ListeningPort.ToString());
             CefClass.Initialize(new DefaultCefSettings
@@ -46,11 +67,9 @@ namespace Sakuno.ING.Browser.Desktop.Cef
 
         public void ClearCache()
         {
-            try
-            {
-                Directory.Delete(cachePath, true);
-            }
-            catch { }
+            this.clearCacheOnStartup.Value = true;
+
+            shellContextService.Capture().ShowMessageAsync("Cache will be cleared on the next startup.", "Information");
         }
         public void ClearCookie() => CefClass.GetGlobalCookieManager().DeleteCookies(null, null);
 
