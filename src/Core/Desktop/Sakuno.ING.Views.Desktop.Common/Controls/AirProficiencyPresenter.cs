4f9bb06dd61516201c0c20103c1814a2d66d5a16
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-10-11 21:48:24 +0800

    Add air proficiency presenter

---
+++ src/Core/Desktop/Sakuno.ING.Views.Desktop.Common/Controls/AirProficiencyPresenter.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+
+namespace Sakuno.ING.Views.Desktop.Controls
+{
+    public class AirProficiencyPresenter : Control
+    {
+        public static DependencyProperty ProficiencyProperty
+            = DependencyProperty.Register(nameof(Proficiency), typeof(int), typeof(AirProficiencyPresenter), new PropertyMetadata(0, Update));
+        public int Proficiency
+        {
+            get => (int)GetValue(ProficiencyProperty);
+            set => SetValue(ProficiencyProperty, value);
+        }
+
+        private readonly Image image = new Image();
+
+        public AirProficiencyPresenter()
+        {
+            AddVisualChild(image);
+        }
+
+        protected override int VisualChildrenCount => 1;
+        protected override Visual GetVisualChild(int index) => image;
+
+        private static readonly Dictionary<int, BitmapImage> bitmapSources = new Dictionary<int, BitmapImage>();
+        private static void Update(DependencyObject d, DependencyPropertyChangedEventArgs e)
+        {
+            var i = (AirProficiencyPresenter)d;
+            var level = (int)e.NewValue;
+
+            if (level == 0)
+            {
+                i.image.Source = null;
+            }
+
+            if (!bitmapSources.TryGetValue(level, out var source))
+            {
+                try
+                {
+                    source = new BitmapImage(new Uri($"pack://application:,,,/Sakuno.ING.Views.Desktop.Common;component/Images/AirProficiency/{level}.png"));
+                    source.Freeze();
+                }
+                catch
+                {
+                    source = null;
+                }
+                bitmapSources[level] = source;
+            }
+
+            i.image.Source = source;
+        }
+    }
+}
--- src/Core/UWP/Sakuno.ING.Views.UWP.Common/Controls/AirProficiencyPresenter.vb
+++ src/Core/UWP/Sakuno.ING.Views.UWP.Common/Controls/AirProficiencyPresenter.vb
@@ -1,5 +1,5 @@
 ﻿Namespace Controls
-    Public Class EquipmentIcon
+    Public Class AirProficiencyPresenter
         Inherits UserControl
 
         Private ReadOnly _image As New Image
@@ -7,23 +7,25 @@
             Content = _image
         End Sub
 
-        Private _id As Integer
-        Public Property Id As Integer
+        Private _proficiency As Integer
+        Public Property Proficiency As Integer
             Get
-                Return _id
+                Return _proficiency
             End Get
             Set(value As Integer)
-                If _id <> value Then
-                    _id = value
-                    If DesignMode.DesignModeEnabled Then
-                        _image.Source = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/Equipment/{value}.png"))
+                If _proficiency <> value Then
+                    _proficiency = value
+                    If value = 0 Then
+                        _image.Source = Nothing
+                    ElseIf DesignMode.DesignModeEnabled Then
+                        _image.Source = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/AirProficiency/{value}.png"))
                     Else
                         Dim stringId = "UseItemIcon_Bitmap_" & value
                         Dim source As Object = Nothing
                         If Application.Current.Resources.TryGetValue(stringId, source) Then
                             _image.Source = DirectCast(source, BitmapImage)
                         Else
-                            Dim s = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/Equipment/{value}.png"))
+                            Dim s = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/AirProficiency/{value}.png"))
                             Application.Current.Resources.Add(stringId, s)
                             _image.Source = s
                         End If
