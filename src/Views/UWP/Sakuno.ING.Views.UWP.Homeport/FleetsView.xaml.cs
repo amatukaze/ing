b3e8ddd091afeca31d9cb7e2fe33b839de3f10eb
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2018-11-10 22:57:38 +0800

    Add fleet status for UWP and change to TabView

--- src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/FleetsView.xaml.cs
+++ src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/FleetsView.xaml.cs
@@ -1,6 +1,8 @@
 ﻿using Sakuno.ING.Game.Models;
 using Sakuno.ING.Shell;
+using Windows.UI;
 using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
 
 namespace Sakuno.ING.Views.UWP.Homeport
 {
@@ -12,6 +14,53 @@ namespace Sakuno.ING.Views.UWP.Homeport
         {
             ViewModel = viewModel;
             this.InitializeComponent();
+        }
+
+        private static Brush StatusColor(FleetStatus status)
+        {
+            switch (status)
+            {
+                case FleetStatus.Empty:
+                    return new SolidColorBrush(Colors.Gray);
+                case FleetStatus.Ready:
+                    return new SolidColorBrush(Colors.SpringGreen);
+                case FleetStatus.Sortie:
+                    return new SolidColorBrush(Colors.Red);
+                case FleetStatus.Expedition:
+                    return new SolidColorBrush(Colors.Aqua);
+                case FleetStatus.Fatigued:
+                case FleetStatus.Repairing:
+                case FleetStatus.Damaged:
+                case FleetStatus.Insufficient:
+                    return new SolidColorBrush(Colors.Orange);
+                default:
+                    return null;
+            }
+        }
+
+        private static string StatusText(FleetStatus status)
+        {
+            switch (status)
+            {
+                case FleetStatus.Empty:
+                    return "Empty fleet";
+                case FleetStatus.Ready:
+                    return "Ready";
+                case FleetStatus.Sortie:
+                    return "In sortie";
+                case FleetStatus.Expedition:
+                    return "In expedition";
+                case FleetStatus.Fatigued:
+                    return "Low morale";
+                case FleetStatus.Repairing:
+                    return "Repairing";
+                case FleetStatus.Damaged:
+                    return "Heavily damaged";
+                case FleetStatus.Insufficient:
+                    return "Supply required";
+                default:
+                    return string.Empty;
+            }
         }
     }
 }
