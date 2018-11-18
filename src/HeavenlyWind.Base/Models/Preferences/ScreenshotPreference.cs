0004045dda1b4283bf13fe88ee24a9687e35a60f
神樹桜乃 <kodama@sakuno.moe>
2018-11-18 14:53:36 +0800

    Use jpeg format as default screenshot format

--- src/HeavenlyWind.Base/Models/Preferences/ScreenshotPreference.cs
+++ src/HeavenlyWind.Base/Models/Preferences/ScreenshotPreference.cs
@@ -4,6 +4,6 @@
     {
         public Property<string> Path { get; } = new Property<string>("browser.screenshot.path", "Screenshots");
         public Property<string> FilenameFormat { get; } = new Property<string>("browser.screenshot.filename_format", "ing_{0:yyyyMMdd-HHmmss-ff}.{1}");
-        public Property<ScreenshotImageFormat> ImageFormat { get; } = new Property<ScreenshotImageFormat>("browser.screenshot.image_format", ScreenshotImageFormat.Png);
+        public Property<ScreenshotImageFormat> ImageFormat { get; } = new Property<ScreenshotImageFormat>("browser.screenshot.image_format2", ScreenshotImageFormat.Jpeg);
     }
 }
