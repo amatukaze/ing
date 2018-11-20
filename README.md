440e191cd8d4d532435e496d19dff64099e92bdc
神樹桜乃 <kodama@sakuno.moe>
2018-11-20 22:02:24 +0800

    Version 0.1.15.11 (Braindrive)

--- README.md
+++ README.md
@@ -3,7 +3,7 @@
 艦これ補助ツール「いんてりじぇんと連装砲くん」（智能型连装炮君 / ING）
 
 ### Information
-Current version: **0.1.15.10 (Braindrive)**
+Current version: **0.1.15.11 (Braindrive)**
 
 [Changelogs](https://github.com/amatukaze/IntelligentNavalGun/wiki/Changelogs) here
 
--- src/HeavenlyWind.Base/ProductInfo.cs
+++ src/HeavenlyWind.Base/ProductInfo.cs
@@ -7,11 +7,11 @@ namespace Sakuno.KanColle.Amatsukaze
         public const string AppName = "いんてりじぇんと連装砲くん";
         public const string ProductName = "Intelligent Naval Gun";
 
-        public const string AssemblyVersionString = "0.1.15.10";
+        public const string AssemblyVersionString = "0.1.15.11";
 
         public static string Version => AssemblyVersionString;
         public static string ReleaseCodeName => "Braindrive";
-        public static string ReleaseDate => "2018.08.20";
+        public static string ReleaseDate => "2018.11.20";
         public static string ReleaseType => "Release";
 
         public const string UserAgent = "ING/" + AssemblyVersionString;
