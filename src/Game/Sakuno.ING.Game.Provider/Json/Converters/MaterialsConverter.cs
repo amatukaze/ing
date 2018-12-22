2fa57972c8960eabeff0a24b6730c8c920ded127
神樹桜乃 <kodama@sakuno.moe>
2018-12-22 21:31:26 +0800

    Fix the materials update of some APIs

--- src/Game/Sakuno.ING.Game.Provider/Json/Converters/MaterialsConverter.cs
+++ src/Game/Sakuno.ING.Game.Provider/Json/Converters/MaterialsConverter.cs
@@ -1,19 +1,51 @@
 ﻿using System;
+using Newtonsoft.Json;
 using Sakuno.ING.Game.Models;
 
 namespace Sakuno.ING.Game.Json.Converters
 {
-    internal class MaterialsConverter : IntArrayConverterBase<Materials>
+    internal class MaterialsConverter : JsonConverter
     {
-        protected override int RequiredCount => 4;
+        public override bool CanConvert(Type objectType) => objectType == typeof(Materials);
+        public override bool CanWrite => false;
+        public override void WriteJson(JsonWriter writer, object value, JsonSerializer serializer) => throw new NotSupportedException();
+        public override object ReadJson(JsonReader reader, Type objectType, object existingValue, JsonSerializer serializer)
+        {
+            Materials result = default;
 
-        protected override Materials ConvertValue(ReadOnlySpan<int> array)
-            => new Materials
+            if (reader.TokenType == JsonToken.StartArray)
             {
-                Fuel = array[0],
-                Bullet = array[1],
-                Steel = array[2],
-                Bauxite = array[3]
-            };
+                for (var i = 0; reader.ReadAsInt32() is int current; i++)
+                    switch (i)
+                    {
+                        case 0:
+                            result.Fuel = current;
+                            break;
+                        case 1:
+                            result.Bullet = current;
+                            break;
+                        case 2:
+                            result.Steel = current;
+                            break;
+                        case 3:
+                            result.Bauxite = current;
+                            break;
+                        case 4:
+                            result.InstantBuild = current;
+                            break;
+                        case 5:
+                            result.InstantRepair = current;
+                            break;
+                        case 6:
+                            result.Development = current;
+                            break;
+                        case 7:
+                            result.Improvement = current;
+                            break;
+                    }
+            }
+
+            return result;
+        }
     }
 }
