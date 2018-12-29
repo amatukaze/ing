b8065111088aa1110832a952a70f6c83d19eaa10
神樹桜乃 <kodama@sakuno.moe>
2018-12-29 19:35:27 +0800

    Enemy ambush may support land-base aerial support

--- src/HeavenlyWind.Game/Models/Battle/Stages/EnemyAmbushStage.cs
+++ src/HeavenlyWind.Game/Models/Battle/Stages/EnemyAmbushStage.cs
@@ -9,14 +9,23 @@ namespace Sakuno.KanColle.Amatsukaze.Game.Models.Battle.Stages
     {
         public override BattleStageType Type => BattleStageType.EnemyAmbush;
 
+        public LandBaseJetAircraftAerialSupport LandBaseJetAircraftAerialSupport { get; protected set; }
+        public LandBaseAerialSupportPhase LandBaseAerialSupport { get; protected set; }
         public ShellingPhase Shelling { get; protected set; }
 
-        public override IList<BattlePhase> Phases => new BattlePhase[] { Shelling };
+        public override IList<BattlePhase> Phases => new BattlePhase[]
+        {
+            LandBaseJetAircraftAerialSupport,
+            LandBaseAerialSupport,
+            Shelling,
+        };
 
         internal protected EnemyAmbushStage(BattleInfo rpOwner, ApiInfo rpInfo) : base(rpOwner)
         {
             var rDay = rpInfo.Data as RawDay;
 
+            LandBaseJetAircraftAerialSupport = new LandBaseJetAircraftAerialSupport(this, rDay.LandBaseJetAircraftAerialSupport);
+            LandBaseAerialSupport = new LandBaseAerialSupportPhase(this, rDay.LandBaseAerialSupport);
             Shelling = new ShellingPhase(this, rDay.ShellingFirstRound);
         }
     }
