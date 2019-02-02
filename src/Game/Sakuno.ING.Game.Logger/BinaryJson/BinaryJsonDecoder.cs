a7b0625a011ebc220ac74b98a8e478f316d4486e
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-02-02 14:43:06 +0800

    Binary json deserializer and test

---
+++ src/Game/Sakuno.ING.Game.Logger/BinaryJson/BinaryJsonDecoder.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+
+namespace Sakuno.ING.Game.Logger.BinaryJson
+{
+    internal class BinaryJsonDecoder
+    {
+        private ReadOnlyMemory<byte> data;
+        public BinaryJsonDecoder(ReadOnlyMemory<byte> data)
+        {
+            this.data = data;
+        }
+
+        private byte ReadByte()
+        {
+            byte value = data.Span[0];
+            data = data.Slice(1);
+            return value;
+        }
+
+        public bool IsNextInteger() => (data.Span[0] & 0x80) == 0;
+        public bool IsNextArray() => (data.Span[0] & 0b1110_0000) == 0b1000_0000;
+        public bool IsNextObject() => (data.Span[0] & 0b1110_0000) == 0b1010_0000;
+        public bool IsNextString() => (data.Span[0] & 0b1110_0000) == 0b1100_0000;
+        public bool IsNextDecimal() => (data.Span[0] & 0b1110_0000) == 0b1110_0000;
+
+        public bool Ends => data.IsEmpty;
+
+        private (bool negative, int value) ReadRemainNumber()
+        {
+            int scale = 0;
+            int result = 0;
+            while (true)
+            {
+                byte lsb = ReadByte();
+                if ((lsb & 0x80) != 0)
+                {
+                    result += (lsb & 0x7F) << scale;
+                    scale += 7;
+                }
+                else
+                    return ((lsb & 0x40) != 0, result + ((lsb & 0x3F) << scale));
+            }
+        }
+
+        private int ReadHeaderNumber()
+        {
+            byte lsb = ReadByte();
+            if ((lsb & 0x10) != 0)
+                return (lsb & 0x0F) | (ReadRemainNumber().value << 4);
+            else
+                return lsb & 0x0F;
+        }
+
+        public int? ReadInteger()
+        {
+            byte lsb = ReadByte();
+            if ((lsb & 0x40) != 0)
+            {
+                (bool neg, int r) = ReadRemainNumber();
+                int n = (lsb & 0x3F) | (r << 6);
+                if (n == 0) return null;
+                return neg ? -n : n;
+            }
+            else
+                return lsb;
+        }
+
+        public decimal ReadDecimal()
+        {
+            int scale = ReadHeaderNumber();
+            (bool neg, decimal value) = ReadRemainNumber();
+            while (scale > 0)
+            {
+                value /= 10;
+                scale--;
+            }
+            return neg ? -value : value;
+        }
+
+        public decimal? ReadNumber()
+        {
+            if (IsNextInteger())
+                return ReadInteger();
+            else if (IsNextDecimal())
+                return ReadDecimal();
+            else
+                return null;
+        }
+
+        public string ReadString()
+        {
+            int length = ReadHeaderNumber();
+            // use span/u8string in new std
+            var bytes = new byte[length];
+            data.CopyTo(bytes);
+            data = data.Slice(length);
+            return Encoding.UTF8.GetString(bytes);
+        }
+
+        public int ReadJName() => ReadRemainNumber().value;
+
+        public int ReadContainerLength() => ReadHeaderNumber();
+
+        public T[] ReadArray<T>(Func<BinaryJsonDecoder, T> func)
+        {
+            int n = ReadContainerLength();
+            var result = new T[n];
+            for (int i = 0; i < n; i++)
+                result[i] = func(this);
+            return result;
+        }
+
+        public T ReadObject<T>(Action<BinaryJsonDecoder, int, T> action)
+            where T : new()
+        {
+            int n = ReadContainerLength();
+            T result = new T();
+            while (n-- > 0)
+                action(this, ReadJName(), result);
+            return result;
+        }
+
+        public void SkipValue()
+        {
+            if (IsNextInteger())
+                ReadInteger();
+            else if (IsNextDecimal())
+                ReadDecimal();
+            else if (IsNextString())
+                ReadString();
+            else if (IsNextArray())
+            {
+                int n = ReadContainerLength();
+                while (n-- > 0)
+                    SkipValue();
+            }
+            else if (IsNextObject())
+            {
+                int n = ReadContainerLength();
+                while (n-- > 0)
+                {
+                    ReadJName();
+                    SkipValue();
+                }
+            }
+        }
+    }
+}
--- tests/Sakuno.ING.Game.Logger.Tests/BinaryJsonTest.cs
+++ tests/Sakuno.ING.Game.Logger.Tests/BinaryJsonTest.cs
@@ -17,11 +17,11 @@ namespace Sakuno.ING.Game.Tests
         {
             var utf8 = Encoding.UTF8.GetBytes(json);
             var result = new BinaryJsonEncoder(utf8, jNames).Result;
-            Assert.Equal(result, value);
+            Assert.Equal(value, result);
         }
 
         [Fact]
-        public static void TestInteger()
+        public static void TestEncodeInteger()
         {
             AssertEncodeEqual("1", new byte[] { 1 });
             AssertEncodeEqual("128", new byte[] { 0b0100_0000, 2 });
@@ -29,14 +29,14 @@ namespace Sakuno.ING.Game.Tests
         }
 
         [Fact]
-        public static void TestDecimal()
+        public static void TestEncodeDecimal()
         {
             AssertEncodeEqual("1.1", new byte[] { 0b1110_0001, 11 });
             AssertEncodeEqual("-1.1", new byte[] { 0b1110_0001, 0b0100_1011 });
         }
 
         [Fact]
-        public static void TestArray()
+        public static void TestEncodeArray()
         {
             AssertEncodeEqual("[1,2,3,4]", new byte[]
             {
@@ -52,7 +52,7 @@ namespace Sakuno.ING.Game.Tests
         }
 
         [Fact]
-        public static void TestObject()
+        public static void TestEncodeObject()
         {
             AssertEncodeEqual("{\"a\":1,\"b\":[1,2]}", new byte[]
             {
@@ -66,6 +66,75 @@ namespace Sakuno.ING.Game.Tests
                 1, 0b0100_0000, 0,
                 2, 0
             });
+        }
+
+        [Fact]
+        public static void TestDecodeInteger()
+        {
+            Assert.Equal(1, new BinaryJsonDecoder(new byte[] { 1 }).ReadInteger());
+            Assert.Equal(-1, new BinaryJsonDecoder(new byte[] { 0b0100_0001, 0b0100_0000 }).ReadInteger());
+            Assert.Equal(128, new BinaryJsonDecoder(new byte[] { 0b0100_0000, 2 }).ReadInteger());
+            for (int i = -512; i <= 512; i++)
+            {
+                var utf8 = Encoding.UTF8.GetBytes(i.ToString());
+                var b = new BinaryJsonEncoder(utf8, jNames).Result;
+                var decoder = new BinaryJsonDecoder(b);
+                Assert.Equal(i, decoder.ReadInteger());
+                Assert.True(decoder.Ends);
+            }
+        }
+
+        [Fact]
+        public static void TestDecodeNull()
+            => Assert.Null(new BinaryJsonDecoder(new byte[] { 0b0100_0000, 0 }).ReadInteger());
+
+        [Fact]
+        public static void TestDecodeDecimal()
+        {
+            for (decimal d = -512.5m; d <= 512; d++)
+            {
+                var utf8 = Encoding.UTF8.GetBytes(d.ToString());
+                var b = new BinaryJsonEncoder(utf8, jNames).Result;
+                var decoder = new BinaryJsonDecoder(b);
+                Assert.Equal(d, decoder.ReadDecimal());
+                Assert.True(decoder.Ends);
+            }
+        }
+
+        [Fact]
+        public static void TestDecodeString()
+            => Assert.Equal("abc", new BinaryJsonDecoder(new byte[] { 0b1100_0011, (byte)'a', (byte)'b', (byte)'c' }).ReadString());
+
+        [Fact]
+        public static void TestDecodeArray()
+        {
+            var d = new BinaryJsonDecoder(new byte[] { 0b1000_0100, 1, 2, 3, 4 });
+            Assert.True(d.IsNextArray());
+            Assert.Equal(4, d.ReadContainerLength());
+            Assert.Equal(1, d.ReadInteger());
+            Assert.Equal(2, d.ReadInteger());
+            Assert.Equal(3, d.ReadInteger());
+            Assert.Equal(4, d.ReadInteger());
+            Assert.True(d.Ends);
+        }
+
+        [Fact]
+        public static void TestDecodeSkip()
+        {
+            void AssertSkip(string original)
+            {
+                var utf8 = Encoding.UTF8.GetBytes(original);
+                var b = new BinaryJsonEncoder(utf8, jNames).Result;
+                var decoder = new BinaryJsonDecoder(b);
+                decoder.SkipValue();
+                Assert.True(decoder.Ends);
+            }
+            AssertSkip("1");
+            AssertSkip("-123.456");
+            AssertSkip("[1,2,[3,4],5]");
+            AssertSkip("{\"a\":1,\"b\":2}");
+            AssertSkip("{\"a\":[1,2],\"b\":[3,4,5]}");
+            AssertSkip("[{\"a\":1,\"b\":2},{\"a\":3,\"b\":4}]");
         }
     }
 }
