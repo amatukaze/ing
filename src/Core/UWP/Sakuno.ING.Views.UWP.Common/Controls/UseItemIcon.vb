56c48a23d50833b6ca5771c6cb5c6091f2edd243
神樹桜乃 <kodama@sakuno.moe>
2019-02-22 00:46:14 +0800

    Change UseItemIcon.Id to dependency property (UWP)

--- src/Core/UWP/Sakuno.ING.Views.UWP.Common/Controls/UseItemIcon.vb
+++ src/Core/UWP/Sakuno.ING.Views.UWP.Common/Controls/UseItemIcon.vb
@@ -10,30 +10,34 @@ Namespace Controls
             Content = _image
         End Sub
 
-        Private _id As KnownUseItem
+        Public Shared ReadOnly IdProperty As DependencyProperty =
+            DependencyProperty.Register(NameOf(Id), GetType(KnownUseItem), GetType(UseItemIcon),
+                                        New PropertyMetadata(New KnownUseItem, Sub(d, e) CType(d, UseItemIcon).OnIdChanged(CType(e.NewValue, KnownUseItem))))
+
         Public Property Id As KnownUseItem
             Get
-                Return _id
+                Return CType(GetValue(IdProperty), KnownUseItem)
             End Get
             Set(value As KnownUseItem)
-                If (_id <> value) Then
-                    _id = value
-                    Dim intId As Integer = value
-                    If DesignMode.DesignModeEnabled Then
-                        _image.Source = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/UseItem/{intId}.png"))
-                    Else
-                        Dim stringId = "UseItemIcon_Bitmap_" & intId
-                        Dim source As Object = Nothing
-                        If Application.Current.Resources.TryGetValue(stringId, source) Then
-                            _image.Source = DirectCast(source, BitmapImage)
-                        Else
-                            Dim s = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/UseItem/{intId}.png"))
-                            Application.Current.Resources.Add(stringId, s)
-                            _image.Source = s
-                        End If
-                    End If
-                End If
+                SetValue(IdProperty, value)
             End Set
         End Property
+
+        Private Sub OnIdChanged(value As KnownUseItem)
+            Dim intId As Integer = value
+            If DesignMode.DesignModeEnabled Then
+                _image.Source = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/UseItem/{intId}.png"))
+            Else
+                Dim stringId = "UseItemIcon_Bitmap_" & intId
+                Dim source As Object = Nothing
+                If Application.Current.Resources.TryGetValue(stringId, source) Then
+                    _image.Source = DirectCast(source, BitmapImage)
+                Else
+                    Dim s = New BitmapImage(New Uri($"ms-appx:///Sakuno.ING.Views.UWP.Common/Assets/Images/UseItem/{intId}.png"))
+                    Application.Current.Resources.Add(stringId, s)
+                    _image.Source = s
+                End If
+            End If
+        End Sub
     End Class
 End Namespace
