a789dee139cc43db85434e38cfa3b1905ffc349f
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-02-23 20:27:00 +0800

    Add ef migration script

---
+++ scripts/ef.ps1
@@ -0,0 +1,22 @@
+param(
+    [Parameter(Mandatory = $true)]
+    [ValidateSet('Setting', 'Log', IgnoreCase = $true)]
+    [string]$Project,
+    [Parameter(Mandatory = $true)]
+    [string]$Verb,
+    [string]$Noun
+)
+
+$current = $PSCommandPath | Split-Path  -Parent | Split-Path -Parent
+switch ($Project) {
+    'Setting' { 
+        $folder = "$current\src\Infrastructure"
+        $projectName = "Sakuno.ING.Data"
+    }
+    'Log' {
+        $folder = "$current\src\Game"
+        $projectName = "Sakuno.ING.Game.Logger"
+    }
+}
+
+dotnet ef migrations $Verb --project "$folder\$projectName\" --startup-project "$folder\$projectName.Design\" --msbuildprojectextensionspath "$current\intermediate\$projectName.Design\" --framework netcoreapp2.2 $Noun
