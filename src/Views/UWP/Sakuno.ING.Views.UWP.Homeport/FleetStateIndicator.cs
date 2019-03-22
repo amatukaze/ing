3076ec3a1711eb68378baeaf2229fc0d84c2d069
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-03-22 21:40:28 +0800

    Fix fleet state indicator

---
+++ src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/FleetStateIndicator.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using Sakuno.ING.Game.Models;
+using Windows.UI;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Media.Animation;
+using Windows.UI.Xaml.Shapes;
+
+namespace Sakuno.ING.Views.UWP.Homeport
+{
+    [TemplatePart(Name = nameof(PART_Shape), Type = typeof(Shape))]
+    internal class FleetStateIndicator : Control
+    {
+        public static readonly DependencyProperty StateProperty
+            = DependencyProperty.Register(nameof(State), typeof(FleetState), typeof(FleetStateIndicator),
+                new PropertyMetadata(FleetState.Empty, (d, e) => ((FleetStateIndicator)d).CheckState()));
+        public FleetState State
+        {
+            get => (FleetState)GetValue(StateProperty);
+            set => SetValue(StateProperty, value);
+        }
+
+        protected override void OnApplyTemplate()
+        {
+            base.OnApplyTemplate();
+            PART_Shape = GetTemplateChild(nameof(PART_Shape)) as Shape;
+            if (PART_Shape != null)
+            {
+                PART_Shape.Fill = brush = new SolidColorBrush();
+                var animation = new DoubleAnimation
+                {
+                    From = 1,
+                    To = 0,
+                    Duration = new Duration(TimeSpan.FromSeconds(0.5)),
+                    AutoReverse = true,
+                    RepeatBehavior = RepeatBehavior.Forever
+                };
+                Storyboard.SetTarget(animation, PART_Shape);
+                Storyboard.SetTargetProperty(animation, nameof(Opacity));
+                storyBoard = new Storyboard
+                {
+                    Children = { animation }
+                };
+            }
+            else
+            {
+                brush = null;
+                storyBoard = null;
+            }
+            CheckState();
+        }
+
+        private Shape PART_Shape;
+        private SolidColorBrush brush;
+        private Storyboard storyBoard;
+        private void CheckState()
+        {
+            if (PART_Shape is null) return;
+            var state = State;
+
+            if (state == FleetState.Warning)
+                storyBoard.Begin();
+            else
+                storyBoard.Stop();
+
+            brush.Color = state switch
+            {
+                FleetState.Empty => Colors.Gray,
+                FleetState.Ready => Colors.SpringGreen,
+                FleetState.Sortie => Colors.Red,
+                FleetState.Warning => Colors.Red,
+                FleetState.Expedition => Colors.Aqua,
+                FleetState.Fatigued => Colors.Orange,
+                FleetState.Repairing => Colors.Orange,
+                FleetState.Damaged => Colors.Orange,
+                FleetState.Insufficient => Colors.Orange,
+                _ => Colors.Transparent
+            };
+        }
+    }
+}
--- src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/FleetsView.xaml.cs
+++ src/Views/UWP/Sakuno.ING.Views.UWP.Homeport/FleetsView.xaml.cs
@@ -1,8 +1,6 @@
 ﻿using Sakuno.ING.Game.Models;
 using Sakuno.ING.Shell;
-using Windows.UI;
 using Windows.UI.Xaml.Controls;
-using Windows.UI.Xaml.Media;
 
 namespace Sakuno.ING.Views.UWP.Homeport
 {
@@ -14,29 +12,6 @@ namespace Sakuno.ING.Views.UWP.Homeport
         {
             ViewModel = viewModel;
             this.InitializeComponent();
-        }
-
-        internal static Brush StatusColor(FleetState status)
-        {
-            switch (status)
-            {
-                case FleetState.Empty:
-                    return new SolidColorBrush(Colors.Gray);
-                case FleetState.Ready:
-                    return new SolidColorBrush(Colors.SpringGreen);
-                case FleetState.Sortie:
-                case FleetState.Warning:
-                    return new SolidColorBrush(Colors.Red);
-                case FleetState.Expedition:
-                    return new SolidColorBrush(Colors.Aqua);
-                case FleetState.Fatigued:
-                case FleetState.Repairing:
-                case FleetState.Damaged:
-                case FleetState.Insufficient:
-                    return new SolidColorBrush(Colors.Orange);
-                default:
-                    return null;
-            }
         }
     }
 }
