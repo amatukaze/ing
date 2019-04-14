a49cd56c895332c5b003356d9946af8aaa3d4a1b
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-04-14 18:14:49 +0800

    Stand out battle overview

---
+++ src/Views/Sakuno.ING.Views.Desktop/Combat/BattleOverview.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace Sakuno.ING.Views.Desktop.Combat
+{
+    public partial class BattleOverview : UserControl
+    {
+        public BattleOverview()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ src/Views/Sakuno.ING.Views.UWP/Combat/BattleOverview.xaml.cs
@@ -0,0 +1,23 @@
+﻿using Sakuno.ING.Game.Models.Combat;
+using Windows.UI.Xaml.Controls;
+
+namespace Sakuno.ING.Views.UWP.Combat
+{
+    public sealed partial class BattleOverview : UserControl
+    {
+        private Battle _battle;
+        public Battle Battle
+        {
+            get => _battle;
+            set
+            {
+                _battle = value;
+                Bindings.Update();
+            }
+        }
+        public BattleOverview()
+        {
+            this.InitializeComponent();
+        }
+    }
+}
