2df65c7c93c0e0b7fb54c18fb26b43846dc6fc3d
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-04-30 14:40:48 +0800

    Add usage hint for uwp

---
+++ src/Core/UWP/Sakuno.ING.UWP/UsageHintView.xaml.cs
@@ -0,0 +1,25 @@
+﻿using Sakuno.ING.Settings;
+using Sakuno.ING.Shell;
+using Windows.ApplicationModel;
+using Windows.Storage;
+using Windows.System;
+using Windows.UI.Xaml.Controls;
+
+namespace Sakuno.ING.UWP
+{
+    [ExportSettingView(SettingCategory.Misc)]
+    public sealed partial class UsageHintView : UserControl
+    {
+        private readonly StorageFolder LocalFolder = ApplicationData.Current.LocalFolder;
+        private readonly string LoopbackCommand = "CheckNetIsolation LoopbackExempt -a -n=" + Package.Current.Id.FamilyName;
+        public UsageHintView()
+        {
+            this.InitializeComponent();
+        }
+
+        private void LaunchDataFolder()
+        {
+            _ = Launcher.LaunchFolderAsync(LocalFolder);
+        }
+    }
+}
