83333cb6406934f1668cde1ff30d46d6bcc6f561
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-08-07 16:40:52 +0800

    Fix consumption reduce for marriage

--- src/Game/Sakuno.ING.Game.Models/Models/Ship.cs
+++ src/Game/Sakuno.ING.Game.Models/Models/Ship.cs
@@ -13,10 +13,11 @@ namespace Sakuno.ING.Game.Models
         {
             using (EnterBatchNotifyScope())
             {
+                bool isMarriaged = Leveling.Level >= 100;
                 SupplyingCost = new Materials
                 {
-                    Fuel = Fuel.Shortage,
-                    Bullet = Bullet.Shortage,
+                    Fuel = isMarriaged ? (int)(Fuel.Shortage * 0.85) : Fuel.Shortage,
+                    Bullet = isMarriaged ? (int)(Bullet.Shortage * 0.85) : Bullet.Shortage,
                     Bauxite = Slots.Sum(x => x.Aircraft.Shortage) * 5
                 };
                 AirFightPower = Slots.Sum(x => x.AirFightPower);
