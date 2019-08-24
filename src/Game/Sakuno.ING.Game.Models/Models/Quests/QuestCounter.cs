6fdcfb8dd8ee8718c944e4e40cd609d9fe94bc58
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-08-24 20:47:48 +0800

    Quest counter class

---
+++ src/Game/Sakuno.ING.Game.Models/Models/Quests/QuestCounter.cs
@@ -0,0 +1,30 @@
+﻿namespace Sakuno.ING.Game.Models.Quests
+{
+    public abstract class QuestCounter : BindableObject
+    {
+        private readonly IStatePersist statePersist;
+        private readonly QuestId questId;
+        private readonly int counterId;
+
+        protected QuestCounter(IStatePersist statePersist, QuestId questId, int maximum, int counterId = 0)
+        {
+            this.statePersist = statePersist;
+            this.questId = questId;
+            this.counterId = counterId;
+            Progress = (statePersist.GetQuestProgress(questId, counterId) ?? 0, maximum);
+        }
+
+        private ClampedValue _progress;
+        public ClampedValue Progress
+        {
+            get => _progress;
+            private set => Set(ref _progress, value);
+        }
+
+        protected void Increase()
+        {
+            Progress += 1;
+            statePersist.SetQuestProgress(questId, counterId, Progress.Current);
+        }
+    }
+}
--- src/Game/Sakuno.ING.Game.Models/Models/Quests/QuestTarget.cs
+++ src/Game/Sakuno.ING.Game.Models/Models/Quests/QuestTarget.cs
@@ -1,6 +1,27 @@
-﻿namespace Sakuno.ING.Game.Models.Quests
+﻿using System.Collections.Generic;
+
+namespace Sakuno.ING.Game.Models.Quests
 {
-    public abstract class QuestTarget
+    public abstract class QuestTarget : BindableObject
     {
+        public abstract IReadOnlyList<QuestCounter> Counters { get; }
+
+        protected void UpdateProgress()
+        {
+            int current = 0, max = 0;
+            foreach (var c in Counters)
+            {
+                current += c.Progress.Current;
+                max += c.Progress.Max;
+            }
+            TotalProgress = (current, max);
+        }
+
+        private ClampedValue _totalProgress;
+        public ClampedValue TotalProgress
+        {
+            get => _totalProgress;
+            private set => Set(ref _totalProgress, value);
+        }
     }
 }
