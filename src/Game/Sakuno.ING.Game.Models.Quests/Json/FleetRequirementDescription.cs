aeed852a4cd755420b6af124fcbf2af21b425835
Huo Yaoyuan <huoyaoyuan@hotmail.com>
2019-09-20 22:51:00 +0800

    Fix quest 257 and 504

---
+++ src/Game/Sakuno.ING.Game.Models.Quests/Json/FleetRequirementDescription.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Immutable;
+using System.Linq;
+using Sakuno.ING.Game.Models.MasterData;
+
+namespace Sakuno.ING.Game.Models.Quests.Json
+{
+    internal class FleetRequirementDescription
+    {
+        public int RequiredCount { get; set; }
+        public int? NoMoreThan { get; set; }
+        public bool RequireFlagship { get; set; }
+        public bool RequireAll { get; set; }
+        public bool NoUpgrade { get; set; }
+        public ImmutableArray<int?> ShipType { get; set; }
+        public ImmutableArray<int> Ship { get; set; }
+
+        public bool Satisfy(Fleet fleet)
+        {
+            bool SatisfyShip(Ship ship)
+            {
+                var info = ship?.Info;
+                if (info is null) return false;
+                foreach (var id in Ship)
+                    if (NoUpgrade ?
+                        info.Id == id :
+                        info.CanUpgradeFrom((ShipInfoId)id))
+                        return true;
+                return false;
+            }
+
+            bool SatisfyType(Ship ship)
+            {
+                var type = ship?.Info?.Type;
+                foreach (var id in ShipType)
+                    if (type?.Id == id)
+                        return true;
+                return false;
+            }
+
+            if (fleet is null) return false;
+            var satisfy = Ship.IsDefault ? (Func<Ship, bool>)SatisfyType : SatisfyShip;
+
+            if (RequireAll)
+                return fleet.Ships.All(satisfy);
+            if (RequireFlagship && !satisfy(fleet.Ships.FirstOrDefault()))
+                return false;
+            int count = fleet.Ships.Count(satisfy);
+            if (count > NoMoreThan)
+                return false;
+            return count >= RequiredCount;
+        }
+    }
+}
--- src/Game/Sakuno.ING.Game.Models.Quests/Json/QuestCounterDescriptionJson.cs
+++ src/Game/Sakuno.ING.Game.Models.Quests/Json/QuestCounterDescriptionJson.cs
@@ -1,8 +1,5 @@
-﻿using System;
-using System.Collections.Immutable;
-using System.Linq;
+﻿using System.Collections.Immutable;
 using Sakuno.ING.Game.Models.Combat;
-using Sakuno.ING.Game.Models.MasterData;
 
 namespace Sakuno.ING.Game.Models.Quests.Json
 {
@@ -18,49 +15,5 @@ namespace Sakuno.ING.Game.Models.Quests.Json
         public ImmutableArray<int> EquipmentType { get; set; }
         public ImmutableArray<int> Equipment { get; set; }
         public ImmutableArray<FleetRequirementDescription> Fleet { get; set; }
-    }
-
-    internal class FleetRequirementDescription
-    {
-        public int RequiredCount { get; set; }
-        public int? NoMoreThan { get; set; }
-        public bool RequireFlagship { get; set; }
-        public bool NoUpgrade { get; set; }
-        public ImmutableArray<int?> ShipType { get; set; }
-        public ImmutableArray<int> Ship { get; set; }
-
-        public bool Satisfy(Fleet fleet)
-        {
-            bool SatisfyShip(Ship ship)
-            {
-                var info = ship?.Info;
-                if (info is null) return false;
-                foreach (var id in Ship)
-                    if (NoUpgrade ?
-                        info.Id == id :
-                        info.CanUpgradeFrom((ShipInfoId)id))
-                        return true;
-                return false;
-            }
-
-            bool SatisfyType(Ship ship)
-            {
-                var type = ship?.Info?.Type;
-                foreach (var id in ShipType)
-                    if (type?.Id == id)
-                        return true;
-                return false;
-            }
-
-            if (fleet is null) return false;
-            var satisfy = Ship.IsDefault ? (Func<Ship, bool>)SatisfyType : SatisfyShip;
-
-            if (RequireFlagship && !satisfy(fleet.Ships.FirstOrDefault()))
-                return false;
-            int count = fleet.Ships.Count(satisfy);
-            if (count > NoMoreThan)
-                return false;
-            return count >= RequiredCount;
-        }
     }
 }
