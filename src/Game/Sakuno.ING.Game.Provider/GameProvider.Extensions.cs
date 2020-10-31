778441265e519577122454aa3c0819d2ece63d2f
神樹桜乃 <kodama@sakuno.moe>
2020-11-01 00:04:13 +0800

    Update OfData()

--- src/Game/Sakuno.ING.Game.Provider/GameProvider.Extensions.cs
+++ src/Game/Sakuno.ING.Game.Provider/GameProvider.Extensions.cs
@@ -6,16 +6,12 @@ namespace Sakuno.ING.Game
 {
     internal static class SvDataObservableExtensions
     {
+        public static IObservable<T> OfData<T>(this IObservable<SvData?> source) =>
+            source.OfType<SvData<T>>().Where(svdata => svdata.api_result == 1).Select(svdata => svdata.api_data);
+
         public static IObservable<TEvent> Parse<TRaw, TEvent>(this IObservable<SvData?> source, Func<TRaw, TEvent> eventSelector)
         {
             var events = source.OfType<SvData<TRaw>>().Where(svdata => svdata.api_result == 1).Select(svdata => eventSelector(svdata.api_data)).Publish();
-            events.Connect();
-
-            return events.AsObservable();
-        }
-        public static IObservable<T> OfData<T>(this IObservable<SvData?> source)
-        {
-            var events = source.OfType<SvData<T>>().Where(svdata => svdata.api_result == 1).Select(svdata => svdata.api_data).Publish();
             events.Connect();
 
             return events.AsObservable();
