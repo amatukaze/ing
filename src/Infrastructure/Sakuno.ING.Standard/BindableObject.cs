9b3268f3ccd9be222664b9889df812d9e4038956
神樹桜乃 <kodama@sakuno.moe>
2020-11-27 00:18:13 +0800

    Add bindable object type

---
+++ src/Infrastructure/Sakuno.ING.Standard/BindableObject.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
+namespace Sakuno.ING
+{
+    public abstract class BindableObject : IBindable
+    {
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        protected void NotifyPropertyChanged([CallerMemberName] string propertyName = null) =>
+            NotifyPropertyChanged(new PropertyChangedEventArgs(propertyName));
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected void NotifyPropertyChanged(PropertyChangedEventArgs args) =>
+            PropertyChanged?.Invoke(this, args);
+
+        protected void Set<T>(ref T field, T value, [CallerMemberName] string propertyName = null)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return;
+
+            field = value;
+            NotifyPropertyChanged(propertyName);
+        }
+        [EditorBrowsable(EditorBrowsableState.Never)]
+        protected void Set<T>(ref T field, T value, PropertyChangedEventArgs args)
+        {
+            if (EqualityComparer<T>.Default.Equals(field, value))
+                return;
+
+            field = value;
+            NotifyPropertyChanged(args);
+        }
+    }
+}
