ca706988f15bc02c87c8b21982ecae66a133e0c9
神樹桜乃 <kodama@sakuno.moe>
2020-12-18 01:35:28 +0800

    Prepare for composition

---
+++ src/Infrastructure/Sakuno.ING.Standard/Composition/Compositor.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+namespace Sakuno.ING.Composition
+{
+    public abstract class Compositor
+    {
+        private static Compositor? _default;
+        public static Compositor Default => _default ?? throw new InvalidOperationException("Not initialized");
+
+        protected Compositor()
+        {
+            _default ??= this;
+        }
+
+        public abstract T Resolve<T>() where T : class;
+        public abstract object Resolve(Type type);
+    }
+}
---
+++ src/Infrastructure/Sakuno.ING.Standard/Composition/ExportAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Sakuno.ING.Composition
+{
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = true, Inherited = false)]
+    public sealed class ExportAttribute : Attribute
+    {
+        public Type? ContractType { get; }
+        public bool SingleInstance { get; set; } = true;
+        public bool LazyCreate { get; set; } = true;
+
+        public ExportAttribute() { }
+        public ExportAttribute(Type contractType) => ContractType = contractType;
+    }
+}
