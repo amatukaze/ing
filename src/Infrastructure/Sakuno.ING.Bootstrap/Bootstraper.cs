7c7b0d3381cadd38cc4ecfb5843a9a3083b41747
神樹桜乃 <kodama@sakuno.moe>
2020-12-19 01:59:50 +0800

    Add bootstrap project

---
+++ src/Infrastructure/Sakuno.ING.Bootstrap/Bootstraper.cs
@@ -0,0 +1,50 @@
+﻿using DryIoc;
+using ReactiveUI;
+using Sakuno.ING.Composition;
+using Sakuno.ING.Shell;
+using Splat;
+using Splat.DryIoc;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+
+namespace Sakuno.ING.Bootstrap
+{
+    public static class Bootstraper
+    {
+        public static void Initialize(IEnumerable<string> assemblyNames)
+        {
+            Initialize(assemblyNames.Select(Assembly.Load));
+        }
+        public static void Initialize(IEnumerable<Assembly> assemblies)
+        {
+            var container = new Container();
+
+            container.UseDryIocDependencyResolver();
+
+            foreach (var assembly in assemblies)
+                foreach (var type in assembly.DefinedTypes)
+                {
+                    if (type.IsAbstract)
+                        continue;
+
+                    foreach (var attribute in type.GetCustomAttributes())
+                        switch (attribute)
+                        {
+                            case ExportAttribute exportAttribute:
+                                container.Register(exportAttribute.ContractType ?? type, type, exportAttribute.LazyCreate ? null : Reuse.Singleton);
+                                break;
+                        }
+                }
+
+            Locator.CurrentMutable.InitializeSplat();
+
+            new DryIocCompositor(container);
+        }
+
+        public static void Startup()
+        {
+            Compositor.Default.Resolve<IShell>().Run();
+        }
+    }
+}
---
+++ src/Infrastructure/Sakuno.ING.Bootstrap/DryIocCompositor.cs
@@ -0,0 +1,19 @@
+﻿using DryIoc;
+using Sakuno.ING.Composition;
+using System;
+
+namespace Sakuno.ING.Bootstrap
+{
+    internal sealed class DryIocCompositor : Compositor
+    {
+        private readonly IContainer _container;
+
+        public DryIocCompositor(Container container)
+        {
+            _container = container;
+        }
+
+        public override T Resolve<T>() => _container.Resolve<T>();
+        public override object Resolve(Type type) => _container.Resolve(type);
+    }
+}
