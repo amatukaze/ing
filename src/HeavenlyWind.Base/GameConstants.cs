314ceea526c5aab549bd41ced68e318daf9eafc9
神樹桜乃 <kodama@sakuno.moe>
2023-03-29 12:59:40 +0800

    Target game page url

--- src/HeavenlyWind.Base/GameConstants.cs
+++ src/HeavenlyWind.Base/GameConstants.cs
@@ -2,7 +2,7 @@
 {
     public static class GameConstants
     {
-        public const string GamePageUrl = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854/";
+        public const string GamePageUrl = "http://www.dmm.com/netgame/social/-/gadgets/=/app_id=854854";
 
         public const double GameWidth = 1200.0;
         public const double GameHeight = 720.0;
