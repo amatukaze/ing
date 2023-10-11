3ac331891ab08095b89d2b147fb55264e887c541
神樹桜乃 <kodama@sakuno.moe>
2023-10-12 07:14:42 +0800

    Always use first LoS formula

--- src/HeavenlyWind/Controls/LoSFormulas.cs
+++ src/HeavenlyWind/Controls/LoSFormulas.cs
@@ -18,7 +18,7 @@ namespace Sakuno.KanColle.Amatsukaze.Controls
             if (rpNewValue == null || Items.Count == 0)
                 return;
 
-            SelectedItem = Items[(int)Preference.Instance.Game.MainFleetLoSFormula.Value];
+            SelectedItem = Items[0];
         }
     }
 }
