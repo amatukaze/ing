08db4cc5b95c2550a0bc6cdbb4d51da78facbfe9
神樹桜乃 <kodama@sakuno.moe>
2023-11-06 11:39:12 +0800

    Add master & player data services

---
+++ src/Game/Sakuno.ING.Game/Services/MasterDataService.cs
@@ -0,0 +1,38 @@
+﻿using Sakuno.ING.Game.Events.MasterData;
+using Sakuno.ING.Game.Models.MasterData;
+using Sakuno.ING.Game.Provider;
+using System.Reactive.Linq;
+
+namespace Sakuno.ING.Game.Services;
+
+public class MasterDataService
+{
+    public ITable<ShipInfo, ShipInfoId> Ships { get; }
+    public ITable<ShipTypeInfo, ShipTypeId> ShipTypes { get; }
+    public ITable<SlotItemInfo, SlotItemInfoId> SlotItems { get; }
+    public ITable<SlotItemTypeInfo, SlotItemTypeId> SlotItemTypes { get; }
+    public ITable<UseItemInfo, UseItemId> UseItems { get; }
+    public ITable<MapAreaInfo, MapAreaId> MapAreas { get; }
+    public ITable<MapInfo, MapId> Maps { get; }
+    public ITable<ExpeditionInfo, ExpeditionId> Expeditions { get; }
+
+    public MasterDataService(IGameProvider gameProvider)
+    {
+        Ships = new Table<ShipInfo, ShipInfoId, IShipInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.Ships));
+        ShipTypes = new Table<ShipTypeInfo, ShipTypeId, IShipTypeInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.ShipTypes));
+        SlotItems = new Table<SlotItemInfo, SlotItemInfoId, ISlotItemInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.SlotItems));
+        SlotItemTypes = new Table<SlotItemTypeInfo, SlotItemTypeId, ISlotItemTypeInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.SlotItemTypes));
+        UseItems = new Table<UseItemInfo, UseItemId, IUseItemInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.UseItems));
+        MapAreas = new Table<MapAreaInfo, MapAreaId, IMapAreaInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.MapAreas));
+        Maps = new Table<MapInfo, MapId, IMapInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.Maps));
+        Expeditions = new Table<ExpeditionInfo, ExpeditionId, IExpeditionInfoUpdated>(
+            gameProvider.MasterDataUpdated.Select(masterData => masterData.Expeditions));
+    }
+}
---
+++ src/Game/Sakuno.ING.Game/Services/PlayerDataService.cs
@@ -0,0 +1,38 @@
+﻿using Sakuno.ING.Game.Events;
+using Sakuno.ING.Game.Models;
+using Sakuno.ING.Game.Models.MasterData;
+using Sakuno.ING.Game.Provider;
+using System.Reactive.Subjects;
+
+namespace Sakuno.ING.Game.Services;
+
+public class PlayerDataService
+{
+    public ITable<Ship, ShipId> Ships { get; }
+    public ITable<SlotItem, SlotItemId> SlotItems { get; }
+
+    public ITable<Fleet, FleetId> Fleets { get; }
+
+    public ITable<ConstructionDock, ConstructionDockId> ConstructionDocks { get; }
+    public ITable<RepairDock, RepairDockId> RepairDocks { get; }
+
+    public ITable<UseItem, UseItemId> UseItems { get; }
+
+    public PlayerDataService(IGameProvider gameProvider)
+    {
+        var removeShipsSubject = new Subject<ShipId[]>();
+
+        Ships = new Table<Ship, ShipId, IShipUpdated>(gameProvider.ShipsUpdated, gameProvider.PartialShipsUpdated, removeShipsSubject);
+
+        var removeSlotItemsSubject = new Subject<SlotItemId[]>();
+
+        SlotItems = new Table<SlotItem, SlotItemId, ISlotItemUpdated>(gameProvider.SlotItemsUpdated, gameProvider.PartialSlotItemsUpdated, removeSlotItemsSubject);
+
+        Fleets = new Table<Fleet, FleetId, IFleetUpdated>(gameProvider.FleetsUpdated, gameProvider.PartialFleetsUpdated);
+
+        ConstructionDocks = new Table<ConstructionDock, ConstructionDockId, IConstructionDockUpdated>(gameProvider.ConstructionDocksUpdated);
+        RepairDocks = new Table<RepairDock, RepairDockId, IRepairDockUpdated>(gameProvider.RepairDocksUpdated);
+
+        UseItems = new Table<UseItem, UseItemId, IUseItemUpdated>(gameProvider.UseItemsUpdated);
+    }
+}
