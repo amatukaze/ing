d77cd01907e42ab26f90aef5bea2379ea317162b
神樹桜乃 <kodama@sakuno.moe>
2024-03-15 08:38:41 +0800

    Update ShipsTests

--- tests/Sakuno.ING.ViewModels.Tests/ShipsTests.cs
+++ tests/Sakuno.ING.ViewModels.Tests/ShipsTests.cs
@@ -1,28 +1,47 @@
-using Sakuno.ING.ViewModels.Homeport;
-
 namespace Sakuno.ING.ViewModels.Tests;
 
 public class ShipsTests
 {
-    [Fact]
-    public void Count()
+    private readonly Subject<IReadOnlyList<IShipUpdated>> _shipsUpdatedSubject = new();
+    private readonly Subject<IReadOnlyList<IShipUpdated>> _partialShipsUpdatedSubject = new();
+    private readonly ShipsViewModel _vm;
+
+    public ShipsTests()
     {
-        var shipsUpdatedSubject = new Subject<IReadOnlyList<IShipUpdated>>();
-        var partialShipsUpdatedSubject = new Subject<IReadOnlyList<IShipUpdated>>();
         var provider = Substitute.For<IGameProvider>();
-        provider.ShipsUpdated.Returns(shipsUpdatedSubject);
-        provider.PartialShipsUpdated.Returns(partialShipsUpdatedSubject);
+        provider.ShipsUpdated.Returns(_shipsUpdatedSubject);
+        provider.PartialShipsUpdated.Returns(_partialShipsUpdatedSubject);
 
-        var vm = new ShipsViewModel(new PlayerDataService(provider));
+        _vm = new ShipsViewModel(new PlayerDataService(provider));
+    }
 
-        Assert.Equal(0, vm.Count);
+    [Fact]
+    public void ZeroCountAtFirst()
+    {
+        Assert.Equal(0, _vm.Count);
+    }
 
-        shipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(1, 3).ToArray());
+    [Fact]
+    public void FullUpdate()
+    {
+        _shipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(1, 4).ToArray());
 
-        Assert.Equal(3, vm.Count);
+        Assert.Equal(4, _vm.Count);
 
-        partialShipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(4, 2).ToArray());
+        _shipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(10, 6).ToArray());
 
-        Assert.Equal(5, vm.Count);
+        Assert.Equal(6, _vm.Count);
+    }
+
+    [Fact]
+    public void PartialUpdate()
+    {
+        _partialShipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(1, 3).ToArray());
+
+        Assert.Equal(3, _vm.Count);
+
+        _partialShipsUpdatedSubject.OnNext(Utils.GenerateMocks<IShipUpdated, ShipId>(2, 5).ToArray());
+
+        Assert.Equal(6, _vm.Count);
     }
 }
