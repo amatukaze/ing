84370e53824b4a619dce71932e6e885cc0a9ab05
神樹桜乃 <kodama@sakuno.moe>
2024-08-01 16:00:08 +0800

    Add generic host initialization

--- src/Shell/Sakuno.ING.Shell.Launcher/App.axaml.cs
+++ src/Shell/Sakuno.ING.Shell.Launcher/App.axaml.cs
@@ -1,15 +1,22 @@
+using System;
 using Avalonia.Controls.ApplicationLifetimes;
 using Avalonia.Data.Core.Plugins;
 using Avalonia.Markup.Xaml;
 using DryIoc;
 using DryIoc.Microsoft.DependencyInjection;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using ReactiveUI;
+using Sakuno.ING.Game;
 using Sakuno.ING.ViewModels;
+using Splat;
+using Splat.DryIoc;
 
 namespace Sakuno.ING.Shell.Launcher;
 
 internal partial class App : Application
 {
+    private IHost _host = default!;
     private IContainer _container = default!;
 
     public override void Initialize()
@@ -19,20 +26,42 @@ internal partial class App : Application
 
     public override void OnFrameworkInitializationCompleted()
     {
+        _host = BuildHost();
+
+        _container = _host.Services.GetRequiredService<IContainer>();
+        _container.UseDryIocDependencyResolver();
+
+        var resolver = Locator.CurrentMutable;
+        resolver.InitializeSplat();
+        resolver.InitializeReactiveUI();
+
         if (ApplicationLifetime is IClassicDesktopStyleApplicationLifetime desktop)
         {
             BindingPlugins.DataValidators.RemoveAt(0);
 
-            var serviceCollection = new ServiceCollection();
-
-            serviceCollection.AddSingleton<MainWindow>();
-            serviceCollection.AddSingleton<MainWindowViewModel>();
-
-            _container = serviceCollection.BuildDryIocServiceProvider().Container;
+            desktop.Exit += (sender, e) =>
+            {
+                _host.StopAsync().GetAwaiter().GetResult();
+            };
 
             desktop.MainWindow = _container.Resolve<MainWindow>();
         }
 
         base.OnFrameworkInitializationCompleted();
+
+        _ = _host.StartAsync();
+    }
+
+    private IHost BuildHost()
+    {
+        var hostBuilder = Host.CreateApplicationBuilder(Environment.GetCommandLineArgs());
+        hostBuilder.ConfigureContainer(new DryIocServiceProviderFactory());
+
+        hostBuilder.Services.AddGameServices();
+
+        hostBuilder.Services.AddSingleton<MainWindowViewModel>();
+        hostBuilder.Services.AddSingleton<MainWindow>();
+
+        return hostBuilder.Build();
     }
 }
