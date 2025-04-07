7a53f86a0ef2dcc99c8adaa1b3fe5fdaa374730f
神樹桜乃 <kodama@sakuno.moe>
2025-04-07 17:10:04 +0800

    Add MapAreaName control

---
+++ src/Shell/Sakuno.ING.Shell.Common/Controls/MapAreaName.cs
@@ -0,0 +1,47 @@
+using Microsoft.Extensions.DependencyInjection;
+using Sakuno.ING.Game.Models.MasterData;
+using Sakuno.ING.Game.Services;
+
+namespace Sakuno.ING.Shell.Controls;
+
+[TemplatePart("PART_TextBlock", typeof(TextBlock), IsRequired = true)]
+public class MapAreaName : TemplatedControl
+{
+    public static readonly StyledProperty<MapAreaId> MapAreaProperty =
+        AvaloniaProperty.Register<MapAreaName, MapAreaId>(nameof(MapArea));
+
+    public MapAreaId MapArea
+    {
+        get => GetValue(MapAreaProperty);
+        set => SetValue(MapAreaProperty, value);
+    }
+
+    private TextBlock? _textBlock;
+
+    protected override void OnApplyTemplate(TemplateAppliedEventArgs e)
+    {
+        base.OnApplyTemplate(e);
+
+        _textBlock = e.NameScope.Find<TextBlock>("PART_TextBlock");
+
+        UpdateName();
+    }
+
+    protected override void OnPropertyChanged(AvaloniaPropertyChangedEventArgs e)
+    {
+        base.OnPropertyChanged(e);
+
+        if (e.Property == MapAreaProperty)
+            UpdateName();
+    }
+
+    private void UpdateName()
+    {
+        if (_textBlock is null)
+            return;
+
+        var mapAreas = DependencyInjection.GetContainer(this).GetRequiredService<MasterDataService>().MapAreas;
+
+        _textBlock.Text = mapAreas.TryGetValue(MapArea, out var areaInfo) ? areaInfo.Name : "?";
+    }
+}
