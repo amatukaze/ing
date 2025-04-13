bbfca81f684355c7558525b3fe267663feaaed87
神樹桜乃 <kodama@sakuno.moe>
2025-04-13 08:41:10 +0800

    Add base class for view model

---
+++ src/ViewModels/Sakuno.ING.ViewModels/ViewModelObject.cs
@@ -0,0 +1,20 @@
+﻿namespace Sakuno.ING.ViewModels;
+
+public abstract class ViewModelObject : BindableObject, IDisposable
+{
+    protected readonly CompositeDisposable Disposables = [];
+
+    public void Dispose()
+    {
+        Dispose(true);
+        GC.SuppressFinalize(this);
+    }
+
+    protected virtual void Dispose(bool disposing)
+    {
+        if (!disposing || Disposables.IsDisposed)
+            return;
+
+        Disposables.Dispose();
+    }
+}
