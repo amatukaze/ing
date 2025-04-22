c4011da102b80a01e73d06cd7c39dffe26636d1c
神樹桜乃 <kodama@sakuno.moe>
2025-04-22 11:34:27 +0800

    Add view locator

---
+++ src/Shell/Sakuno.ING.Shell/ViewLocator.cs
@@ -0,0 +1,18 @@
+﻿using Avalonia.Controls.Templates;
+using Sakuno.ING.ViewModels;
+using ViewModelViewHost = Sakuno.ING.Shell.Controls.ViewModelViewHost;
+
+namespace Sakuno.ING.Shell;
+
+internal class ViewLocator : IDataTemplate
+{
+    public bool Match(object? data) => data is ViewModelObject;
+
+    public Control Build(object? param)
+    {
+        if (param is null)
+            return new TextBlock() { Text = "(null)" };
+
+        return new ViewModelViewHost() { ViewModel = param };
+    }
+}
