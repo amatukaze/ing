07704c8363747b49ec76482d9518691a0977266d
神樹桜乃 <kodama@sakuno.moe>
2025-10-18 00:21:37 +0800

    Update product info

--- src/HeavenlyWind.Base/ProductInfo.cs
+++ src/HeavenlyWind.Base/ProductInfo.cs
@@ -7,11 +7,11 @@ namespace Sakuno.KanColle.Amatsukaze
         public const string AppName = "いんてりじぇんと連装砲くん";
         public const string ProductName = "Intelligent Naval Gun";
 
-        public const string AssemblyVersionString = "1.16.0";
+        public const string AssemblyVersionString = "1.17.0";
 
         public static string Version => AssemblyVersionString;
         public static string ReleaseCodeName => "Braindrive";
-        public static string ReleaseDate => "2024.03.07";
+        public static string ReleaseDate => "2025.10.18";
         public static string ReleaseType => "Beta";
 
         public const string UserAgent = "ING/" + AssemblyVersionString;
