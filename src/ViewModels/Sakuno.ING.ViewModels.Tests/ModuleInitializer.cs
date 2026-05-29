d27e787cb35284db18814190a616e976ee597b56
神樹桜乃 <kodama@sakuno.moe>
2026-05-30 02:19:14 +0800

    Fix main thread scheduler in vm unit tests

---
+++ src/ViewModels/Sakuno.ING.ViewModels.Tests/ModuleInitializer.cs
@@ -0,0 +1,17 @@
+﻿using System.Runtime.CompilerServices;
+using ReactiveUI;
+using ReactiveUI.Builder;
+
+namespace Sakuno.ING.ViewModels.Tests;
+
+public static class ModuleInitializer
+{
+    [ModuleInitializer]
+    public static void Initialize()
+    {
+        RxAppBuilder.CreateReactiveUIBuilder()
+            .BuildApp();
+
+        RxSchedulers.MainThreadScheduler = new TestScheduler();
+    }
+}
