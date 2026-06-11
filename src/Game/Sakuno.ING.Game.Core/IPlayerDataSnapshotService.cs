fcbba2923e05c39fcff1c49c106fcdcd428de0a5
神樹桜乃 <kodama@sakuno.moe>
2026-06-12 02:19:38 +0800

    Add PlayerDataSnapshotService

---
+++ src/Game/Sakuno.ING.Game.Core/IPlayerDataSnapshotService.cs
@@ -0,0 +1,17 @@
+﻿using Sakuno.ING.Game.Models;
+using Sakuno.ING.Game.Models.MasterData;
+
+namespace Sakuno.ING.Game;
+
+public interface IPlayerDataSnapshotService
+{
+    ITableSnapshot<Ship, ShipId> Ships { get; }
+    ITableSnapshot<SlotItem, SlotItemId> SlotItems { get; }
+
+    ITableSnapshot<Fleet, FleetId> Fleets { get; }
+
+    ITableSnapshot<ConstructionDock, ConstructionDockId> ConstructionDocks { get; }
+    ITableSnapshot<RepairDock, RepairDockId> RepairDocks { get; }
+
+    ITableSnapshot<UseItem, UseItemId> UseItems { get; }
+}
---
+++ src/Game/Sakuno.ING.Game/Services/PlayerDataSnapshotService.cs
@@ -0,0 +1,15 @@
+﻿using Sakuno.ING.Game.Models;
+using Sakuno.ING.Game.Models.MasterData;
+
+namespace Sakuno.ING.Game.Services;
+
+[RegisterSingleton(Registration = RegistrationStrategy.SelfWithProxyFactory)]
+internal class PlayerDataSnapshotService(PlayerDataService service) : IPlayerDataSnapshotService
+{
+    public ITableSnapshot<Ship, ShipId> Ships => service.Ships.Snapshot;
+    public ITableSnapshot<SlotItem, SlotItemId> SlotItems => service.SlotItems.Snapshot;
+    public ITableSnapshot<Fleet, FleetId> Fleets => service.Fleets.Snapshot;
+    public ITableSnapshot<ConstructionDock, ConstructionDockId> ConstructionDocks => service.ConstructionDocks.Snapshot;
+    public ITableSnapshot<RepairDock, RepairDockId> RepairDocks => service.RepairDocks.Snapshot;
+    public ITableSnapshot<UseItem, UseItemId> UseItems => service.UseItems.Snapshot;
+}
