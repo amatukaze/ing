b3bf372b5df18e76e50708f303530f0e1d37c4ee
神樹桜乃 <kodama@sakuno.moe>
2026-06-12 01:37:02 +0800

    Set console output encoding to UTF-8

--- src/Shell/Sakuno.ING.Shell.Launcher/Program.cs
+++ src/Shell/Sakuno.ING.Shell.Launcher/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Text;
 using Avalonia;
 
 namespace Sakuno.ING.Shell.Launcher;
@@ -6,9 +7,13 @@ namespace Sakuno.ING.Shell.Launcher;
 internal sealed class Program
 {
     [STAThread]
-    public static void Main(string[] args) =>
+    public static void Main(string[] args)
+    {
+        Console.OutputEncoding = Encoding.UTF8;
+
         BuildAvaloniaApp()
             .StartWithClassicDesktopLifetime(args);
+    }
 
     public static AppBuilder BuildAvaloniaApp() =>
         AppBuilder.Configure<App>()
