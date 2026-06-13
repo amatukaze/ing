b043c5708a27deaa59cd7ae14e3f2131439c4143
神樹桜乃 <kodama@sakuno.moe>
2026-06-13 11:47:42 +0800

    Exclude specific slot items in counting

--- src/ViewModels/Sakuno.ING.ViewModels/Homeport/SlotItemCountViewModel.cs
+++ src/ViewModels/Sakuno.ING.ViewModels/Homeport/SlotItemCountViewModel.cs
@@ -3,12 +3,23 @@
 [RegisterScoped(Registration = RegistrationStrategy.Self)]
 public class SlotItemCountViewModel : ViewModelObject
 {
+    private readonly HashSet<SlotItemInfoId> _excludedMasterIds =
+    [
+        SlotItemInfoId.From(42),
+        SlotItemInfoId.From(43),
+        SlotItemInfoId.From(145),
+        SlotItemInfoId.From(146),
+        SlotItemInfoId.From(150),
+        SlotItemInfoId.From(241),
+    ];
+
     public IObservable<int> Count { get; }
     public IObservable<int> MaxCount { get; }
 
     public SlotItemCountViewModel(PlayerDataService playerDataService)
     {
         Count = playerDataService.SlotItems.Connect()
+            .Filter(item => !_excludedMasterIds.Contains(item.MasterId))
             .Count()
             .StartWith(0)
             .ObserveOn(RxSchedulers.MainThreadScheduler);
