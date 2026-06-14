080e0d8dcd040326b1860adaee6e4dc308ac1c7c
神樹桜乃 <kodama@sakuno.moe>
2026-06-15 00:11:16 +0800

    Enhance ObservableCollector

--- src/ViewModels/Sakuno.ING.ViewModels.Tests/ObservableCollector.cs
+++ src/ViewModels/Sakuno.ING.ViewModels.Tests/ObservableCollector.cs
@@ -1,24 +1,15 @@
 ﻿namespace Sakuno.ING.ViewModels.Tests;
 
-public class ObservableCollector<T> : IObserver<T>
+public sealed class ObservableCollector<T> : IObserver<T>
 {
     private readonly List<T> _values = new();
     public IReadOnlyList<T> Values => _values;
 
-    public T LatestValue { get; private set; } = default!;
+    public T? LatestValue => _values.Count > 0 ? _values[^1] : default;
+    public Exception? LatestError { get; private set; }
+    public bool IsCompleted { get; private set; }
 
-    void IObserver<T>.OnNext(T value)
-    {
-        _values.Add(value);
-
-        LatestValue = value;
-    }
-
-    void IObserver<T>.OnError(Exception error)
-    {
-    }
-
-    void IObserver<T>.OnCompleted()
-    {
-    }
+    void IObserver<T>.OnNext(T value) => _values.Add(value);
+    void IObserver<T>.OnError(Exception error) => LatestError = error;
+    void IObserver<T>.OnCompleted() => IsCompleted = true;
 }
