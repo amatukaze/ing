0ac39ae13afe1dd9282d14cf795c59c3f878b1ee
神樹桜乃 <kodama@sakuno.moe>
2026-06-19 01:43:44 +0800

    Add GameData<T> to wrap single data observable

---
+++ src/Game/Sakuno.ING.Game/GameData.cs
@@ -0,0 +1,23 @@
+﻿using System.Reactive.Subjects;
+
+namespace Sakuno.ING.Game;
+
+public sealed class GameData<T> : IObservable<T>, IDisposable
+{
+    private readonly ReplaySubject<T> _subject = new(1);
+
+    private readonly IDisposable _subscription;
+
+    public GameData(IObservable<T> source)
+    {
+        _subscription = source.Subscribe(_subject);
+    }
+
+    public IDisposable Subscribe(IObserver<T> observer) => _subject.Subscribe(observer);
+
+    public void Dispose()
+    {
+        _subscription.Dispose();
+        _subject.Dispose();
+    }
+}
